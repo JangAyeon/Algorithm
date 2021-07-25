086e5208ef049e423d9dfb0375c6c1bb4e05af51
jangayeon <ayeon3052@email.com>
2021-07-25 10:57:01 +0900

    210724 2-8 lec

--- 코드구현력기르기/뒤집은소수.py
+++ 코드구현력기르기/뒤집은소수.py
@@ -25,6 +25,30 @@ for x in M:
     if isPrime(rev_x):
         print(rev_x,end=" ")
 
-    
+
+#code from lecture
+def reverse(x):
+    res=0
+    while x>0:
+        t=x%10
+        res=res*10+t
+        x//=10
+    return res
+
+def isPrime(x):
+    if x==1:
+        return False
+    for i in range(2,x//2+1):
+        if x%i==0:
+            return False
+    else:
+        return True
+
+n=int(input())
+a=list(map(int,input()))
+for x in a:
+    tmp=reverse(x)    
+    if isPrime(tmp):
+        print(tmp, end=" ")
     
 
