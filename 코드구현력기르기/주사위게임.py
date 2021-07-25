d01a00d013c7f13eed54881c53f566317b821b7c
jangayeon <ayeon3052@email.com>
2021-07-25 11:03:39 +0900

    210724 lec

--- 코드구현력기르기/주사위게임.py
+++ 코드구현력기르기/주사위게임.py
@@ -21,3 +21,25 @@ for _ in range(N):
     res.append(money(arr))
 
 print(max(res))
+
+#code from lecture
+n=int(input())
+res=0
+for i in range(n):
+    tmp=input().split()
+    tmp.sort()
+    a,b,c=map(int,tmp)
+
+    if a==b and b==c:
+        money=10000+a*1000
+    elif a==b or a==c:
+        money=1000+a*100
+    elif b==c:
+        money=100+b*100
+    else:
+        money=c*100
+        
+    if money>res:
+        res=money
+print(res)
+
