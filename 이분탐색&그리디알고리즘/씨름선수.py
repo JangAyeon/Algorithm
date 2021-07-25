eb4e01e56111a14a1da2372800301e0dd59d9e03
jangayeon <ayeon3052@email.com>
2021-07-25 16:27:31 +0900

    210725 4-6 lec

--- 이분탐색&그리디알고리즘/씨름선수.py
+++ 이분탐색&그리디알고리즘/씨름선수.py
@@ -21,3 +21,20 @@ for i in range(n):
 
 print(cnt)
 
+#code from lecture
+n=int(input())
+body=[]
+for i in range(n):
+    a,b=map(int,input().split())
+    body.append((a,b))
+
+body.sort(reverse=True) #키 기준으로 내림 차순
+largest=0
+cnt=0
+
+for x,y in body:
+    if y>largest: #키는 졌지만 몸무게는 이김
+        largest=y
+        cnt+=1
+
+print(cnt)
