66e7b4a7c404c7476a93a8f0a64381850069e396
jangayeon <ayeon3052@email.com>
2021-07-26 22:13:37 +0900

    210726 3-1 lec

--- 탐색&시뮬레이션/회문문자열검사.py
+++ 탐색&시뮬레이션/회문문자열검사.py
@@ -14,3 +14,26 @@ for i in range(n):
         print("#",i+1," YES",sep="")
 
 
+#code from lecture (1)
+n=int(input())
+for i in range(n):
+    s=input()
+    s=s.upper()
+    size=len(s)
+    for j in range(size//2):
+        if s[j]!=s[-(1+j)]:
+            print("#%d NO" %(i+1))
+            break
+    else:
+        print("#%d YES" %(i+1))
+
+
+#code from lecture (2)
+n=int(input())
+for i in range(n):
+    s=input()
+    s=s.upper()
+    if s==s[::-1]:
+        print("#%d YES" %(i+1))
+    else:
+        print("#%d NO" %(i+1))
