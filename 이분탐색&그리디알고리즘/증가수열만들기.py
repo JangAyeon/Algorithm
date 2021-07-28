c6f923579eae17c1b5bdffca2ed0803e4efa4b65
jangayeon <ayeon3052@ewhain.net>
2021-07-29 01:24:54 +0900

    2107238 4-9 lec

--- 이분탐색&그리디알고리즘/증가수열만들기.py
+++ 이분탐색&그리디알고리즘/증가수열만들기.py
@@ -33,9 +33,37 @@ while start<=end:
             ans.append(arr[end])
             end-=1
         break
-
-
-
             
 print(len(ans[1:]))
 print(res)
+
+#code from lecture
+n=int(input())
+a=list(map(int,input().split()))
+lt=0
+rt=n-1
+last=0
+res=""
+tmp=[]
+while lt<=rt:
+    if a[lt]>last:
+        tmp.append(a[lt],"L")
+    if a[rt]>last:
+        tmp.append(a[rt],"R")
+    #lt값과 rt값 모두 last보다 큰 경우 더 작은 값 넣어야 함
+    tmp.sort()
+    if len(tmp)==0:
+        #더이상 가져올 것이 없는 경우
+        break
+    else:
+        res+=tmp[0][1]
+        last=tmp[0][0]
+        if tmp[0][1]=="L":
+            lt+=1
+        else:
+            rt-=1
+
+    tmp.clear()
+print(len(res))
+print(res)
+
