ef179145616d41f6c7f5ad11667b5a17a76f32e7
jangayeon <ayeon3052@ewhain.net>
2021-07-29 00:45:13 +0900

    210728 4-8 lec

--- 이분탐색&그리디알고리즘/침몰하는타이타닉.py
+++ 이분탐색&그리디알고리즘/침몰하는타이타닉.py
@@ -20,3 +20,22 @@ for i in range(len(arr)):
 
 
 print(cnt)
+
+#code from lecture
+n,limit=map(int,input().split())
+p=list(map(int,input().split()))
+p.sort()
+cnt=0
+while p:
+    if len(p)==1: 
+        #한 명만 남은 경우는 그냥 limit 따지지 않고 탈출
+        cnt+=1
+        break
+    if p[0]+p[-1]>limit:
+        p.pop() #가장 무거운 사람 탈출
+        cnt+=1
+    else:
+        p.pop(0)
+        p.pop()
+        cnt+=1
+print(cnt)
