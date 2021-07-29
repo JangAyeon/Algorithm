41203fa56976a6d646bef97553d870c7036c0bb1
jangayeon <ayeon3052@ewhain.net>
2021-07-30 01:32:52 +0900

    210729 4-10 lec

--- 이분탐색&그리디알고리즘/역수열.py
+++ 이분탐색&그리디알고리즘/역수열.py
@@ -30,7 +30,28 @@ for i in range(len(a)):
     print("arr:",arr)
            
 
+print(arr)
+
+
+#code from lecture
+n=int(input())
+a=list(map(int,input().split()))
+seq=[0]*n
+for i in range(n):
+    for j in range(n):
+        if a[i]==0 and seq[j]==0: 
+            #자기 자리 찾음 
+            #앞에 더 큰 수 갯수 만족 & 빈공간 찾음
+            seq[j]=(i+1)
+            break
+        elif seq[j]==0:
+            #아직 자기 자리 못 찾음
+            #자신보다 큰 숫자 자리 확보 아직 다 못함
+            a[i]-=1
+
+for x in seq:
+    print(x,end=" ")
     
 
-print(arr)
+
 
