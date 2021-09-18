2b60cf848a30ce7f0d8b504905de4aa3ff52e27f
JangAyeon <ayeon3052@ewhain.net>
2021-09-18 10:43:46 +0900

    210918 6-1 pre/lec/rev

--- 완전탐색DFS_기초/재귀함수를이용한이진수출력.py
+++ 완전탐색DFS_기초/재귀함수를이용한이진수출력.py
@@ -1,31 +1,29 @@
 #code before lecture
 
-#n=int(input())
-#res=""
+n=int(input())
+res=""
 
-#while(n!=0):
-#    n,r=divmod(n,2) #몫과 나머지 반환
-#    res+=str(r)
+while(n!=0):
+    n,r=divmod(n,2) #몫과 나머지 반환
+    res+=str(r)
     
-#print(int(res[::-1]))
+print(int(res[::-1]))
 
 #code from lecture
-#def DFS(x):
-#    if x==0:
-#        return;
-#    else:
-#        DFS(x//2)
-#        print(x%2,end=" ")
+def DFS(x):
+    if x==0:
+        return;
+    else:
+        DFS(x//2)
+        print(x%2,end=" ")
 
 
-#if__name__=="__main__":
-#    n=int(input())
-#    DSF(n)
+if__name__=="__main__":
+    n=int(input())
+    DSF(n)
 
 
 #code after lecture
-import sys
-sys.stdin=open("in5.txt","r")
 
 def DFS(n):
     if n!=0:
--- 완전탐색DFS_기초/재귀함수와스택.py
+++ 완전탐색DFS_기초/재귀함수와스택.py
@@ -1,9 +1,9 @@
 #code from lecture
-
 def DFS(x):
     if x>0:
-        print(x)
+       
         DFS(x-1)
+        print(x)
         
 
 if __name__=="__main__":
