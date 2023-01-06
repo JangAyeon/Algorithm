c0f2ab6116b18220ba84bd918f77329db10de8cb
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2023-01-06 22:04:30 +0900

    23.01.06

--- 카카오/2018-2-2.py
+++ 카카오/2018-2-2.py
@@ -1,17 +1,27 @@
+# 2018 KAKAO BLIND RECRUITMENT [1차] 다트 게임
+
 import re
 
 def solution(dartResult):
     ans=[]
     pattern = re.compile(r'([0-9]|10)([SDT])([\*\#]?)')
     token = pattern.findall(dartResult)
+    # [('1', 'S', ''), ('2', 'D', '*'), ('3', 'T', '')]
+
+    cal = {
+        "S": lambda val : val,
+        "D" : lambda val : val**2,
+        "T": lambda val : val**3
+    }
+    
     for i, j, k in token:
-        print(i,j,k)
+       #print(i,j,k)
         if j == "S":
-            tmp = int(i)
+            tmp = cal["S"](int(i))
         elif j == "D":
-            tmp = int(i)**2
+            tmp = cal["D"](int(i))
         elif j == "T":
-            tmp = int(i)**3
+            tmp = cal["T"](int(i))
         if k=="*":
             tmp *=2
             if ans:
@@ -22,4 +32,4 @@ def solution(dartResult):
     #print(ans)
     return sum(ans)
 
-print(solution("1S2D*3T"))
+#print(solution("1S2D*3T"))
