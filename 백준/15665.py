e4b7bbb1ce17d25a0c298e7ea1e463b4a2d146b7
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2023-02-16 15:00:03 +0900

    23.02.16

---
+++ 백준/15665.py
@@ -0,0 +1,21 @@
+# 문제 : https://www.acmicpc.net/problem/15665
+
+import sys
+input= sys.stdin.readline
+n,m = map(int, input().split())
+arr=sorted(list(set(map(int, input().split()))))
+#print(n,m,arr)
+ans=[]
+
+
+def solution(cnt):
+    if cnt==m:
+        print(*ans)
+        return
+    cnt+=1
+    for i in arr:
+        ans.append(i)
+        solution(cnt)
+        ans.pop()
+
+solution(0)
