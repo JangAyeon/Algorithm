724b5ad69738690bc698ffa7834a583688c35aa1
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2023-02-19 11:52:30 +0900

    23.0219

---
+++ 백준/1759.py
@@ -0,0 +1,24 @@
+import sys
+input = sys.stdin.readline
+
+L,C = map(int, input().split())
+arr = sorted(list(input().split()))
+print(L,C,arr)
+
+def solution(len, idx, c_cnt, v_cnt, text):
+    if len == L:
+        if v_cnt>=1 and c_cnt>=2:
+            print(text)
+        return
+    else:
+        for i in range(idx, C):
+            if arr[i] in ('a', 'e', 'i', 'o', 'u'):
+                solution(len+1, i+1, c_cnt, v_cnt+1, text+arr[i])
+            else:
+                solution(len+1, i+1, c_cnt+1, v_cnt, text+arr[i])
+
+
+
+solution(0, 0, 0, 0, "")
+
+
