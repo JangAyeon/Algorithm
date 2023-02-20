f6bf3b130d846489c63c5436b21e518c2de73ee2
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2023-02-20 19:58:05 +0900

    23.02.20

--- 백준/15661.py
+++ 백준/15661.py
@@ -1,35 +1,27 @@
-# https://www.acmicpc.net/problem/15661
-
 import sys
 input = sys.stdin.readline
 from itertools import combinations
 
-n= int(input().strip())
+n = int(input().strip())
 arr = [list(map(int, input().split())) for _ in range(n)]
-idx = [x for x in range(n)]
+idx = list(range(n))
 ans = 2147000000
-#print(n, arr, idx, ans)
 
-
-def get_diff(start, link):
-    stats_start = 0
-    stats_link = 0
-    
-    for i, j in combinations(start, 2):
-        stats_start += arr[i][j] + arr[j][i]
-    for i, j in combinations(link, 2):
-        stats_link += arr[i][j] + arr[j][i]
-    return abs(stats_start - stats_link)
-
+def getDiff(start, link):
+    s_score=0
+    l_score=0
+    for x,y in combinations(start,2):
+        s_score+=arr[x][y]+arr[y][x]
+    for x,y in combinations(link,2):
+        l_score+=arr[x][y]+arr[y][x]
+    return abs(s_score-l_score)
 
 for i in range(1, n//2+1):
-    #print(i)
-    start=tuple(combinations(idx, i))
+    start =  tuple(combinations(idx, i))
     link = tuple(combinations(idx, n-i))
-    size=len(start)
-    #print(start, link)
-    for j in range(size):
-        diff=get_diff(start[j], link[size-1-j])
-        ans=min(ans, diff)
-
+    #print("start", start)
+    #print("link", link)
+    length = len(start)
+    for i in range(length):
+        ans = min(ans, getDiff(start[i], link[length-1-i]))
 print(ans)
--- 백준/test.py
+++ 백준/test.py
@@ -1,58 +1,28 @@
-"""
-YYYYY
-SYSYS
-YYYYY
-YSYYS
-YYYYY
-"""
 import sys
-input=sys.stdin.readline
-from collections import deque
+input = sys.stdin.readline
+from itertools import combinations
 
-arr = [input().strip() for _ in range(5)]
-#print(arr)
-count = 0
-dx=[1,-1,0,0]
-dy=[0,0,1,-1]
+n = int(input().strip())
+arr = [list(map(int, input().split())) for _ in range(n)]
+idx = list(range(n))
+ans = float("inf")
 
+def getDiff(start, link):
+    score={"start":0, "link":0}
+    for x,y in combinations(start,2):
+        score["start"]+=arr[x][y]+arr[y][x]
+    for x,y in combinations(link,2):
+        score["link"]+=arr[x][y]+arr[y][x]
+    return abs(score["start"]-score["link"])
 
-def checkS(group): # 이다솜파 적어도 4명
-    s_cnt = 0
-    for x,y in group:
-        if arr[x][y]=="S":
-            s_cnt+=1
-    return False if s_cnt<4 else True
-
-def checkAdj(group): # 서로 가로 또는 세로로 인접
-    visited=[False]*7 # 방문 여부
-    que = deque()
-    que.append(group[0])
-    visited[0]=True
-
-    while que:
-        x,y=que.popleft()
-        for i in range(4): #가로 세로 탐사
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if (nx,ny) in group: # 해당 좌표가 조합 내 있음 
-                idx = group.index((nx,ny))
-                if not visited[idx]: # 아직 방문 전임
-                    visited[idx]=True
-                    que.append((nx,ny))
-
-    return False if False in visited else True
-
-
-
-def solution(level, start, group):
-    global count
-    if level==7: # 조합 완성됨
-        if checkAdj(group) and checkS(group): #조건 만족하는 경우 갯수 증가
-            count+=1
-        return
-    for i in range(start, 25): #조합 만들기 - 재귀 이용
-        solution(level+1, i+1, group+[divmod(i,5)])
-
-
-solution(0,0,[])
-print(count)
+for i in range(1, n//2+1):
+    start =  tuple(combinations(idx, i))
+    link = tuple(combinations(idx, n-i))
+    #print("start", start)
+    #print("link", link)
+    length = len(start)
+    for i in range(length):
+        _min=getDiff(start[i], link[length-1-i])
+        if ans>_min:
+            ans=_min
+print(ans)
