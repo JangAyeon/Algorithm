be0ebf966084d522ab45bd76e19ac30cfc03c74e
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2023-02-21 15:41:41 +0900

    23.02.21

--- 백준/test.py
+++ 백준/test.py
@@ -1,31 +1,35 @@
 n = int(input())
-b = input().split()
-visited = [False] * 10 # 0 ~ 9
-mx,mn ="",""
+arr = input().split()
+visited = [False] * 10 # 0~9
+mn,mx="",""
+
+
 
 def possible(i,j,k):
     if k=="<":
         return i<j
     elif k==">":
         return i>j
-
-def solution(count, s):
-    global mn,mx
-    if count == n+1:
-        if len(mn)==0:
-            mn=s
-        else:
-            mx=s
-        return
     
-    for i in range(10): # 0~9
+def solution(count, res):
+    global mn,mx
+
+    if count==n+1:
+        if len(mn)==0:
+            mn=res
+        else:
+            mx=res
+        return
+
+    for i in range(10): # 0 ~ 9
         if not visited[i]:
-            if count == 0 or possible(s[-1], str(i), b[count-1]):
+            if count ==0 or possible(res[-1], str(i), arr[count-1]):
                 visited[i]=True
-                solution(count+1, s+str(i))
+                solution(count+1, res+str(i))
                 visited[i]=False
 
 
-solution(0,"")
+solution(0, "")
 print(mx)
 print(mn)
+
