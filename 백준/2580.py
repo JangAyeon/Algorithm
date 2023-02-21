502eb306c7059ed1eb4275d05841915c26bd9c8b
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2023-02-21 15:11:46 +0900

    23.02.21

--- 백준/2580.py
+++ 백준/2580.py
@@ -2,45 +2,43 @@ import sys
 input = sys.stdin.readline
 
 arr = [list(map(int, input().split())) for _ in range(9)]
-zero = [(x, y) for x in range(9) for y in range(9) if arr[x][y] == 0]
+zero = [(x,y) for x in range(9) for y in range(9) if arr[x][y] == 0]
+#print(arr, zero)
 
-def check_x(n, x):
-    for i in range(9):
-        if arr[x][i] == n:
+
+def check_x(i, x):
+    for y in range(9):
+        if arr[x][y] == i:
             return False
     return True
 
 
-def check_y(n, y):
-    for i in range(9):
-        if arr[i][y] == n:
+def check_y(i, y):
+    for x in range(9):
+        if arr[x][y] == i:
             return False
     return True
 
-
-def check_rect(x, y, n):
-    x = x // 3 * 3
-    y = y // 3 * 3
-    for i in range(3):
-        for j in range(3):
-            if arr[x + i][y + j] == n:
+def check_rect(i,x,y):
+    x = x//3*3
+    y=y//3*3
+    for dx in range(3):
+        for dy in range(3):
+            if arr[x+dx][y+dy]==i:
                 return False
     return True
 
-
 def dfs(cnt):
     if cnt == len(zero):
-        for i in range(9):
-            print(*arr[i])
+        for row in arr:
+            print(*row)
         exit()
     x = zero[cnt][0]
     y = zero[cnt][1]
     for i in range(1, 10):
-        if check_y(i, y) and check_rect(x, y, i) and check_x(i, x):
+        if check_y(i, y) and check_rect(i, x, y) and check_x(i, x):
             arr[x][y] = i
             dfs(cnt + 1)
             arr[x][y] = 0 # i가 정답 아닐 수도 있으니 0으로!
 
-
-#print(" ")
 dfs(0)
--- 백준/test.py
+++ 백준/test.py
@@ -1,27 +1,44 @@
 import sys
 input = sys.stdin.readline
-from itertools import combinations
 
-n = int(input().strip())
-arr = [list(map(int, input().split())) for _ in range(n)]
-idx = list(range(n))
-ans = 2147000000
+arr = [list(map(int, input().split())) for _ in range(9)]
+zero = [(x,y) for x in range(9) for y in range(9) if arr[x][y] == 0]
+#print(arr, zero)
 
-def getDiff(start, link):
-    s_score=0
-    l_score=0
-    for x,y in combinations(start,2):
-        s_score+=arr[x][y]+arr[y][x]
-    for x,y in combinations(link,2):
-        l_score+=arr[x][y]+arr[y][x]
-    return abs(s_score-l_score)
 
-for i in range(1, n//2+1):
-    start =  tuple(combinations(idx, i))
-    link = tuple(combinations(idx, n-i))
-    #print("start", start)
-    #print("link", link)
-    length = len(start)
-    for i in range(length):
-        ans = min(ans, getDiff(start[i], link[length-1-i]))
-print(ans)
+def check_x(n, x):
+    for i in range(9):
+        if arr[x][i] == n:
+            return False
+    return True
+
+
+def check_y(n, y):
+    for i in range(9):
+        if arr[i][y] == n:
+            return False
+    return True
+
+def check_rect(i,x,y):
+    x = x//3*3
+    y=y//3*3
+    for dx in range(3):
+        for dy in range(3):
+            if arr[x+dx][y+dy]==i:
+                return False
+    return True
+
+def dfs(cnt):
+    if cnt == len(zero):
+        for i in range(9):
+            print(*arr[i])
+        exit()
+    x = zero[cnt][0]
+    y = zero[cnt][1]
+    for i in range(1, 10):
+        if check_y(i, y) and check_rect(i, x, y) and check_x(i, x):
+            arr[x][y] = i
+            dfs(cnt + 1)
+            arr[x][y] = 0 # i가 정답 아닐 수도 있으니 0으로!
+
+dfs(0)
