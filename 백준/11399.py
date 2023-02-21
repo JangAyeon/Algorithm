ccde96f3fbfc7414be55205177440e8fb73ca47f
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2023-02-21 14:15:02 +0900

    23.02.21

---
+++ 백준/11399.py
@@ -0,0 +1,15 @@
+# 문제 : https://www.acmicpc.net/problem/11399
+
+import sys
+input = sys.stdin.readline
+
+n = int(input().strip())
+arr = sorted(list(map(int, input().split())))
+res = 0
+
+for i in range(0,len(arr)):
+    #print(arr[:i+1])
+    res+=sum(arr[:i+1])
+
+
+print(n, arr,res)
