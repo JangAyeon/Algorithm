e9726fdcebfbe28a96f8fa80dbc2256f4d1cd83e
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2023-02-26 19:09:03 +0900

    23.02.26

--- 백준/1043.py
+++ 백준/1043.py
@@ -8,9 +8,9 @@ story = [1]*m # 1 : 과장된 이야기, 0 : 진실된 이야기
 
 for _ in range(m):
     for idx, i in enumerate(party):
-        if know & i:
-            story[idx]=0
-            know = know | i # 집합 추가
+        if know & i: # 교집합 : know.intersection(i) 와 동일
+            story[idx]=0 # 진실 알게 됨
+            know = know | i # 합집합 : know.union(i)
             #print(know)
         #print(idx, i)
 
