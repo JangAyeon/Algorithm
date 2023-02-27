852b7f181d92604370a1f1258658fd016efc1588
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2023-02-27 15:42:30 +0900

    23.02.27

--- 백준/test.py
+++ 백준/test.py
@@ -1,39 +1,49 @@
 import sys
+from collections import deque 
 input = sys.stdin.readline
 
 n,m = map(int, input().split())
-arr = [list(map(int, input().split())) for _ in range(n)]
-dx=[1,-1,0,0]
-dy=[0,0,1,-1]
-visited = [[False]*m for _ in range(n)]
-max_pos = max(map(max, arr))
+arr = [list(map(int, input().split()) ) for _ in range(n)]
+dx,dy = [1,-1,0,0],[0,0,1,-1]
+
+melted = []
 ans = 0
 
-def dfs(x, y, step, total):
-    global ans
-    if ans>=total+max_pos*(3-step):
-        return 
-    if step == 3:
-        ans = max(ans, total)
-    else:
+
+def bfs():
+    que = deque()
+    visited = [[0]*m for _ in range(n)]
+    que.append([0,0])
+    visited[0][0]=1
+    while que:
+        x,y = que.popleft()
         for i in range(4):
-            nx, ny = x + dx[i], y+dy[i]
+            nx,ny = x+dx[i],y+dy[i]
             if 0<=nx<n and 0<=ny<m and not visited[nx][ny]:
-                if step==1:
-                    visited[nx][ny]=True
-                    dfs(x,y,step+1,total+arr[nx][ny])
-                    visited[nx][ny]=False
-                visited[nx][ny]=True
-                dfs(nx,ny,step+1,total+arr[nx][ny])
-                visited[nx][ny]=False
+                if arr[nx][ny]:
+                    arr[nx][ny]+=1
+                else:
+                    visited[nx][ny] = 1
+                    que.append([nx,ny])
 
 
 
 
-for x in range(len(arr)):
-    for y in range(len(arr[x])):
-        visited[x][y]=True
-        dfs(x,y,0, arr[x][y])
-        visited[x][y]=False
+while True:
+    flag = False
+    bfs()
 
-print(ans)
+    for x in range(n):
+        for y in range(m):
+            if arr[x][y]>=3:
+                arr[x][y]=0
+            elif arr[x][y]>=1:
+                arr[x][y]=1
+                flag = True
+    ans+=1
+
+    if not flag:
+        print(ans)
+        break
+
+
