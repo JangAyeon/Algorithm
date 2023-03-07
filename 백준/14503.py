38c9e6c1fa4f269da1347b41dc6490a6371c997f
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2023-03-07 11:30:47 +0900

    23.03.07

--- 백준/14503.py
+++ 백준/14503.py
@@ -2,33 +2,29 @@ import sys
 input = sys.stdin.readline
 
 
+n,m = map(int, input().split())
+r,c,d = map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(n)]
 dr = [-1, 0, 1, 0]
 dc = [0, 1, 0, -1]
 
-n, m = map(int, input().split())
-r, c, d = map(int, input().split())
-graph = [list(map(int, input().split())) for _ in range(n)]
-
-graph[r][c] = -1
 count = 1
+graph[r][c]=-1
 
-while graph[r][c] != 1:
-    temp = False
+while graph[r][c]!=1:
+    flag = False
     for _ in range(4):
-        d -= 1
-        if d == -1:
-            d = 3
-        nr = r + dr[d]
-        nc = c + dc[d]
+        d = (d+3)%4
+        nr, nc = r + dr[d], c + dc[d]
         if graph[nr][nc] == 0:
+            graph[nr][nc] = -1
             r = nr
             c = nc
-            graph[r][c] = -1
             count += 1
-            temp = True
+            flag = True
             break
-    if not temp:
-        r += dr[d-2]
-        c += dc[d-2]
-        
+    if not flag: # 더이상 청소할 곳이 없음
+        r += dr[d-2]  # 한 칸 후진 
+        c += dc[d-2] # 한 칸 후진
+
 print(count)
