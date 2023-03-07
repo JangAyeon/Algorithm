89e0c4c1f58699487d7fcf5252bb59c65e09341a
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2023-03-07 10:47:07 +0900

    23.03.07

--- 백준/14503.py
+++ 백준/14503.py
@@ -1,48 +1,34 @@
-# 참고 : https://resilient-923.tistory.com/164
-
 import sys
 input = sys.stdin.readline
-"""
-11 10
-7 4 0
-1 1 1 1 1 1 1 1 1 1
-1 0 0 0 0 0 0 0 0 1
-1 0 0 0 1 1 1 1 0 1
-1 0 0 1 1 0 0 0 0 1
-1 0 1 1 0 0 0 0 0 1
-1 0 0 0 0 0 0 0 0 1
-1 0 0 0 0 0 0 1 0 1
-1 0 0 0 0 0 1 1 0 1
-1 0 0 0 0 0 1 1 0 1
-1 0 0 0 0 0 0 0 0 1
-1 1 1 1 1 1 1 1 1 1
-"""
-n,m = map(int, input().split())
-r,c,d = map(int, input().split())
-arr = [list(map(int, input().split())) for _ in range(n)]
 
-visited = [[0]*m for _ in range(n)]
-dr = [-1,0,1,0]
-dc = [0,1,0,-1]
 
-visited[r][c]=1
-cnt=1
+dr = [-1, 0, 1, 0]
+dc = [0, 1, 0, -1]
 
-while True:
-    flag = False
+n, m = map(int, input().split())
+r, c, d = map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(n)]
+
+graph[r][c] = -1
+count = 1
+
+while graph[r][c] != 1:
+    temp = False
     for _ in range(4):
-        d = (d+3)%4
-        nr,nc = r + dr[d], c + dc[d]
-        if 0<=nr<m and 0<=nc<n and not arr[nr][nc] and not visited[nr][nc]:
-            visited[nr][nc]=1
-            cnt+=1
-            r=nr
-            c=nc
-            flag = True
+        d -= 1
+        if d == -1:
+            d = 3
+        nr = r + dr[d]
+        nc = c + dc[d]
+        if graph[nr][nc] == 0:
+            r = nr
+            c = nc
+            graph[r][c] = -1
+            count += 1
+            temp = True
             break
-    if not flag:
-        if arr[r-dr[d]][c-dc[d]]:
-            print(cnt)
-            break
-        else:
-            r,c = r-dr[d],c-dc[d]
+    if not temp:
+        r += dr[d-2]
+        c += dc[d-2]
+        
+print(count)
