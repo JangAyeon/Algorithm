f36a5357631b1acca3e82b863bb282390e5ee671
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2023-03-21 01:10:55 +0900

    2011

--- 백준/2011.py
+++ 백준/2011.py
@@ -1,10 +1,20 @@
 import sys
 input=sys.stdin.readline
-n= input().strip()
-dp = [[0]*(n+1)]
+code = [0]+list(map(int,input().strip()))
+l = len(code)
+dp = [0]*(l)
 dp[0],dp[1]=1,1
 
-for i in range(2,n+1):
-    if 1<=n[i]<=9:
-        dp[i]+=dp[i-1]
-    if 10
+
+def solution():
+    if code[1]==0:
+        return 0
+    else:
+        for i in range(2, l):
+            first = int(code[i])
+            tenth = int(code[i-1])*10 + int(code[i])
+            if first > 0: dp[i] += dp[i-1]
+            if tenth >= 10 and tenth <= 26: dp[i] += dp[i-2]
+        return dp[-1]%1000000
+
+print(solution())
