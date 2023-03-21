0e87a04d3c08c8997615a342d0fc19472d79653e
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2023-03-21 15:22:32 +0900

    합승택시요금

--- 프로그래머스/합승택시요금.py
+++ 프로그래머스/합승택시요금.py
@@ -1,39 +1,34 @@
-# 합승 택시 요금
-
-
 INF = 40000000
 
-def floyd(dist, n):
+def floyd(n, graph):
+    
     for k in range(n):
         for i in range(n):
             for j in range(n):
-                if dist[i][j] > dist[i][k]+dist[k][j]:
-                    dist[i][j] = dist[i][k]+dist[k][j]
-                    
-                    
-def graph(n, fares):
-    graph = [[INF for _ in range(n)] for _ in range(n)] 
+                if graph[i][j] > graph[i][k]+graph[k][j]:
+                    graph[i][j] = graph[i][k]+graph[k][j]
+
+
+def drawGraph(n, fares):
+    graph = [[INF for _ in range(n)] for _ in range(n)]
     
     for i in range(n):
         graph[i][i]=0
     
-    for edges in fares:
-        graph[edges[0]-1][edges[1]-1] = edges[2]
-        graph[edges[1]-1][edges[0]-1] = edges[2]
+    for c, d, f in fares:
+        graph[c-1][d-1]=graph[d-1][c-1]=f
+    
     return graph
 
 def solution(n, s, a, b, fares):
+    graph = drawGraph(n,fares)
+    floyd(n,graph)
     
-    ## 그래프 생성
-    dist = graph(n, fares)
-        
-    ## 구간 간의 최소값 구하기
-    floyd(dist, n)
-    
-    s-=1 # 0 indexing
-    a-=1 # 0 indexing
-    b-=1 # 0 indexing
+    a-=1
+    b-=1
+    s-=1
     answer = INF
-    for k in range(n): ## 합승 끝나는 구간
-        answer = min(answer, dist[s][k]+dist[k][a]+dist[k][b])
+    for c in range(n):
+        answer = min(answer, graph[s][c]+graph[c][b]+graph[c][a])
+    
     return answer
