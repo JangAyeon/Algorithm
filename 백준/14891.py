9c97b0c8e8417d39b8eee1bd399e96798995cc35
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2023-03-26 23:26:04 +0900

    백준: 톱니바퀴 - 참고한거 주석 추가

--- 백준/14891.py
+++ 백준/14891.py
@@ -1,20 +1,20 @@
 import sys
 input = sys.stdin.readline
 from collections import deque
-# https://inspirit941.tistory.com/entry/Python-%EB%B0%B1%EC%A4%80-14891-%ED%86%B1%EB%8B%88%EB%B0%94%ED%80%B4
 
-arr = [deque(map(int, list(input().strip()))) for _ in range(4)]
+
+arr = [deque(map(int, list(input().strip()))) for _ in range(4)] # ❤️ deque로 입력 받아야 나중에 회전 처리하기 쉬움
 # N극은 0, S극은 1로 나타나있다.
 k = int(input().strip())
 
 cmd = []
 for _ in range(k):
     idx, direction = map(int, input().split())
-    cmd.append([idx-1, direction])
+    cmd.append([idx-1, direction]) # ❤️ 0 - indexing 처리 (앞으로 반복문 처리 및 인덱싱을 위해)
 # 첫 번째 정수는 회전시킨 톱니바퀴의 번호, 두 번째 정수는 방향이다. 방향이 1인 경우는 시계 방향이고, -1인 경우는 반시계 방향이다.
 
 def checkLeft(idx, direction):
-    if idx<0 or arr[idx][2]==arr[idx+1][6]:
+    if idx<0 or arr[idx][2]==arr[idx+1][6]: # 재귀 중지 조건
         return
     else:
         # 재귀적으로 바로 다음 왼쪽도 회전 가능한지
@@ -23,7 +23,7 @@ def checkLeft(idx, direction):
         #print("rotate Light Side")
 
 def checkRight(idx, direction):
-    if idx>3 or arr[idx-1][2]==arr[idx][6]:
+    if idx>3 or arr[idx-1][2]==arr[idx][6]: # 재귀 중지 조건 
         return
     else:
         # 바로 오른쪽도 회전 가능한지
