aae05ccef3601d5aa908bc42c3928a58d572c1c1
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2023-03-28 11:17:28 +0900

    boj: 19538

--- 백준/19538.py
+++ 백준/19538.py
@@ -1,46 +1,44 @@
-## https://www.acmicpc.net/problem/19538
+# 백준 루머
+# https://www.acmicpc.net/problem/19538
+
+
 
 import sys
 input = sys.stdin.readline
-from collections import deque 
+from collections import deque
 
-n = int(input())
+
+
+n = int(input().strip())
 graph = [0]
-visited = [0] * (n+1)
-ans = [-1] * (n+1)
-for i in range(n):
-    *a, b = map(int, input().split())
+for _ in range(n):
+    *a, b =map(int, input().split())
     graph.append(a)
-m = int(input())
-start=list(map(int, input().split()))
+m = int(input().strip())
+start = list(map(int,input().split()))
+#print(graph, start)
 
-que = deque()
+visited = [0]*(n+1)
 cnt = [0]*(n+1)
+ans = [-1]*(n+1)
+que = deque()
+
 for i in start:
-    visited[i]=1
-    ans[i]=0
     que.append(i)
-    
+    ans[i]=0
+    visited[i]=1
 
 
 def bfs():
     while que:
-        print(ans)
         current = que.popleft()
         for next in graph[current]:
             if not visited[next]:
-                cnt[next]+=1 #
-                print(next, "방문안함")
-                if cnt[next] >= (len(graph[next])+1)//2:
-                    print("큐 추가", ans[current])
-                    visited[next]=1
-                    ans[next] = ans[current]+1
+                cnt[next]+=1
+                if cnt[next]>=(len(graph[next])+1)//2:
+                    ans[next]=ans[current]+1
                     que.append(next)
-            print("친구 : ",next, ans)
-    
-
-print("입력 받기")
+                    visited[next]=1
 
 bfs()
 print(*ans[1:])
-
--- 백준/test.py
+++ 백준/test.py
@@ -1,53 +1,44 @@
+# 백준 루머
+# https://www.acmicpc.net/problem/19538
+
+
+
 import sys
 input = sys.stdin.readline
 from collections import deque
 
-n=int(input())
-graph = [[0]*n for _ in range(n)] # 0 : 뱀 없음, 1 : 사과 있음, 2 : 방문
-
-k = int(input())
-for _ in range(k):
-    a,b = map(int, input().split())
-    graph[a-1][b-1]=1
-
-l = int(input())
-times = {}
-for _ in range(l):
-    a, b = input().split()
-    times[int(a)]=b
-
-dx = [-1,0,1,0]
-dy = [0,1,0,-1]
-
-def rotation(direction, cmd):
-    if cmd  == "D":
-        direction = (direction+1)%4
-    else:
-        direction = (direction-1)%4
-    return direction
-
-def solution():
-    direction = 1
-    x, y = 0, 0
-    graph[x][y]=2
-    visited = deque([[x,y]])
-    time = 1 
-
-    while True:
-        nx, ny = x + dx[direction], y + dy[direction]
-        if 0<=nx<n and 0<=ny<n and graph[nx][ny]!=2:
-            if graph[nx][ny]!=1: # 사과 없음
-                temp_x, temp_y = visited.popleft()
-                graph[temp_x][temp_y] = 0 # 자리빼기
-            if time in times.keys():
-                direction  = rotation(direction, times[time])
-            graph[nx][ny]=2
-            visited.append([nx,ny])
-            time+=1
-            x, y = nx, ny
-        else:
-            return time
 
 
+n = int(input().strip())
+graph = [0]
+for _ in range(n):
+    *a, b =map(int, input().split())
+    graph.append(a)
+m = int(input().strip())
+start = list(map(int,input().split()))
+#print(graph, start)
 
-print(solution())
+visited = [0]*(n+1)
+cnt = [0]*(n+1)
+ans = [-1]*(n+1)
+que = deque()
+
+for i in start:
+    que.append(i)
+    visited[i]=1
+    ans[i]=0
+
+
+def bfs():
+    while que:
+        current = que.popleft()
+        for next in graph[current]:
+            if not visited[next]:
+                cnt[next]+=1
+                if cnt[next]>=(len(graph[next])+1)//2:
+                    ans[next]=ans[current]+1
+                    que.append(next)
+                    visited[next]=1
+
+bfs()
+print(ans[1:])
