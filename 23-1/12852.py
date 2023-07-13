446a06e71588a048c007820910af0cc5f668b664
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2023-07-13 17:50:54 +0900

    [boj] 12852 complete

--- 23-1/12852.py
+++ 23-1/12852.py
@@ -2,46 +2,29 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-dp = [[1,1] for _ in range(N+1)]
+dp = [[0,0]]*(N+1) 
 
-def default(N):
-    print(1)
-    print(N, 1)
+def getDP(N):
 
-def getDp(N):
-    for i in range(4, len(dp)):
-        temp = []
-        #print("#",i)
-        if (i%3==0):
-            #print("3")
-            temp.append([dp[i//3][0]+1,i//3])
-        if (i%2==0):
-            #print(2)
-            temp.append([dp[i//2][0]+1,i//2])
-        #print(1)
-        temp.append([dp[i-1][0]+1,i-1]) 
-        #print(temp, min(temp))
-        dp[i] = min(temp)
-    print(dp[N][0])
-
-def printDp(N):
-    idx= N
-    print(N, end=" ")
-    while True:
-        n = dp[idx][1]
-        print(n, end=" ")
-        idx = n
-        if n == 1:
-            break
-            
-       
-if N == 2 or N==3:
-    default(N)
-elif N ==1:
-    print(0)
-    print(1)
-else:
-    getDp(N)
-    #print(dp)
-    printDp(N)
+    for i in range(2, N+1):
+        dp[i] = [dp[i-1][0]+1, i-1]
+        if(i%3==0 and dp[i][0]>dp[i//3][0]+1):
+            dp[i] = [dp[i//3][0]+1, i//3]
+        if(i%2==0 and dp[i][0]>dp[i//2][0]+1):
+            dp[i] = [dp[i//2][0]+1, i//2]
+    return dp
     
+def solution(N):
+    dp = getDP(N)
+    result = [0,[]]
+    result[0] = dp[N][0]
+    
+    tmp = N
+    while tmp!=0:
+        result[1].append(tmp)
+        tmp=dp[tmp][1]
+    return result
+
+answer = solution(N)
+print(answer[0])
+print(*answer[1])
