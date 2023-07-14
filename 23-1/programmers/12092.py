8581ddac5120cf278b7775a4be58a8391f9c5c1c
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2023-07-14 18:56:35 +0900

    [prog] 3Xn 타일링 : complete

--- 23-1/programmers/12092.py
+++ 23-1/programmers/12092.py
@@ -9,28 +9,33 @@ def solution(n):
 #12, 2131
 #14, 7953
     
-    temp = [[0,2] for _ in range(n+1)]
-    temp[2]=[0,3]
+
+    temp = [0 for _ in range(n+1)]
+    temp[2]=3
     
-    for idx in range(4,len(temp),2):
-        #print("##",idx)
-        ans = []
-        for i in range(2, idx+2,2):
-            a = sum(temp[i])
-            b = temp[idx-i][-1]
-            #print(i,",",idx-i,":",a,b)
-            if idx-i == 0:
-                ans.append(2)
+    for idx in range(4, n+1,2):
+        #print("idx: ",idx)
+        ans = 0
+        for i in range(2,idx+1,2):
+            #print(i)
+            if i+2 == idx:
+                #print(i,"3배",3*temp[i],temp[i])
+                ans+=3*temp[i]
             else:
-                ans.append(a*b)
-        temp[idx]=ans
-        
-    #print(temp[-1])
-    answer = sum(temp[-1])%1000000007
+                #print(i, "2배",2*temp[i])
+                ans+=2*temp[i]
+        temp[idx] = ans+2
+        #print(temp)
+    answer = temp[n]%1000000007
     return answer
     
 
 
+            
+            
+            
+        
+        
             
             
             
