34a3ba1592dda53868c0f8bb1bff0625b6aeb3bc
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2023-07-18 02:30:26 +0900

    [boj] 64061 complete-2

--- 23-1/programmers/64061.py
+++ 23-1/programmers/64061.py
@@ -1,27 +1,22 @@
-# https://school.programmers.co.kr/learn/courses/30/lessons/64061
-
 def solution(board, moves):
     
-    answer = []
-    count=0
+    bucket = []
+    count = 0
+    
     for i in moves:
-        
         for j in range(len(board[i-1])):
             num = board[j][i-1]
-            #print("##move##",i,num, answer,count)
-            if num!=0:
+            
+            if num !=0:
+                bucket.append(num)
+                board[j][i-1]=0
                 
-                if len(answer)>0 and num==answer[-1]:
-                    del answer[-1]
-                    board[j][i-1] = 0
-                    count+=2
-                else:
-                    board[j][i-1] = 0
-                    answer.append(num)
+                if len(bucket)>1:
+                    if bucket[-1]==bucket[-2]:
+                        bucket.pop(-1)
+                        bucket.pop(-1)
+                        count+=2
                 break
-            else:
-                continue
-                    
-
-    #print(count)
+    
     return count
+    
