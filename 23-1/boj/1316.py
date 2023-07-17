18ffc67cc040da8bb7ed3d7763c4b9b8bc01091e
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2023-07-18 02:07:37 +0900

    [boj] 1316 complete-2

--- 23-1/boj/1316.py
+++ 23-1/boj/1316.py
@@ -1,2 +1,17 @@
-N = int(input())
-cnt = N
+import sys
+input = sys.stdin.readline
+
+N = int(input().strip())
+
+words = [input().strip() for _ in range(N)]
+
+count=len(words)
+
+for word in words:
+    for j in range(1, len(word)):
+        if word[j-1]!=word[j] and word[j] in word[:j]:
+            #print(word[j-1],word[j], word[:j])
+            count -=1
+            break
+
+print(count)
