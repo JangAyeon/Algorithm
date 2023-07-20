c0af82a353096cde50c07857c3567ed53327d7d8
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2023-07-20 14:09:48 +0900

    [boj] 10825 complete

---
+++ 23-1/boj/10825.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input().strip())
+array = []
+for _ in range(N):
+    str = input().strip().split()
+    array.append([str[0], int(str[1]),int(str[2]), int(str[3])])
+
+
+array.sort(key = lambda x : (-x[1],x[2],-x[3],x[0]))
+
+for a in array:
+    print(a[0])
