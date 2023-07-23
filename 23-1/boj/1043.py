437da5e96c254760e84917d0482cba72692effc2
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2023-07-23 16:21:10 +0900

    [boj] 1043 complet

--- 23-1/boj/1043.py
+++ 23-1/boj/1043.py
@@ -2,20 +2,22 @@ import sys
 input = sys.stdin.readline
 
 n, m = map(int, input().strip().split())
-tArray = set(map(int, input().strip().split()[1:]))
-# 0 인 경우 tArray는 빈 리스트로 나옴 
-party = []
-answer = 0
+know = set(map(int, input().split()[1:]))
+party = [set(map(int, input().split()[1:])) for _ in range(m)]
 
-for i in range(m):
-    i = set(map(int,  input().strip().split()[1:]))
-    party.append(i)
-    for j in i:
-        if j in tArray: # 진실 아는 사람 있음 
-            tArray = tArray|i
 
-for j in party:
-    intersection  = j & tArray
-    if not(intersection): answer+=1
+
+
+for _ in range(m):
+    for v  in party:
+        if (v & know):
+            know = know | v
+
+answer  = 0
+for p in party:
+    if p & know:
+        continue
+    else:
+        answer+=1
 
 print(answer)
