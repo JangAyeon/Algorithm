080576eec2e1f64783b96a2feb481b525f09e2f9
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2023-07-23 16:30:20 +0900

    [boj] 1043 add comment

--- 23-1/boj/1043.py
+++ 23-1/boj/1043.py
@@ -7,9 +7,10 @@ party = [set(map(int, input().split()[1:])) for _ in range(m)]
 
 
 
-
-for _ in range(m):
+# 거짓말 여부 확인하고 매번 다시 파티 전체 iteration 돌게 함
+for _ in range(m): 
     for  p  in party:
+        print(p, know)
         if (p & know):
             know = know | p
 
