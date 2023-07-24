dc79d6d9d0551f55417790ae1ee9541f43342035
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2023-07-24 16:26:54 +0900

    [boj] 11057: using summation

--- 23-1/boj/11057.py
+++ 23-1/boj/11057.py
@@ -1,21 +1,13 @@
 import sys
 input = sys.stdin.readline
 
-N=int(input().strip()) 
+answer=[[1]*10]
+n = int(input())
 
-temp = [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]
-
-
-for row in range(1, N+1):
-    array = []
-    for col in range(0, len(temp[row-1])):
-        #print("row - 1 : ",row-1,temp[row-1])
-        #print("row : ",row,temp[row])
-        a = sum(temp[row-1][:col+1])
-        array.append(a)
-        #print(temp[col], col, a )
-    temp.append(array)
-        #temp[row][col] = a
-        #temp[row][col] = sum(temp[row-1][:col+1])
-#print(temp)
-print(temp[N][-1]% 10007)
+for i in range(1,n):
+    temp = []
+    for j in range(0, 10):
+        #print(i,j, sum(answer[i-1][:j+1]))
+        temp.append(sum(answer[i-1][:j+1]))
+    answer.append(temp)
+print(sum(answer[-1])% 10007)
