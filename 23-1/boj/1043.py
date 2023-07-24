58943fb8bab67963c28fc900e116e5854fdbdecf
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2023-07-24 14:34:29 +0900

    [boj] 10825: 입력에서 strip 제거

--- 23-1/boj/1043.py
+++ 23-1/boj/1043.py
@@ -4,21 +4,15 @@ input = sys.stdin.readline
 n, m = map(int, input().strip().split())
 know = set(map(int, input().split()[1:]))
 party = [set(map(int, input().split()[1:])) for _ in range(m)]
-
+lie = [1]*m
 
 
 # 거짓말 여부 확인하고 매번 다시 파티 전체 iteration 돌게 함
 for _ in range(m): 
-    for  p  in party:
-        print(p, know)
+    for idx, p  in enumerate(party):
         if (p & know):
+            lie[idx] = 0
             know = know | p
 
-answer  = 0
-for p in party:
-    if p & know:
-        continue
-    else:
-        answer+=1
 
-print(answer)
+print(sum(lie))
--- 23-1/boj/10825.py
+++ 23-1/boj/10825.py
@@ -1,10 +1,11 @@
 import sys
 input = sys.stdin.readline
 
-N = int(input().strip())
+N = int(input())
 array = []
+
 for _ in range(N):
-    str = input().strip().split()
+    str = input().split() # split 한 상태로 바로 추가 X : 점수만 따로 숫자 처리해야 함 
     array.append([str[0], int(str[1]),int(str[2]), int(str[3])])
 
 
@@ -12,3 +13,4 @@ array.sort(key = lambda x : (-x[1],x[2],-x[3],x[0]))
 
 for a in array:
     print(a[0])
+
