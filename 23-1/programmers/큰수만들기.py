c4f23b96ef4f60a4ceccb1f47c905906628795c5
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2023-07-25 13:57:25 +0900

    [proj] 큰수만들기: solved

--- 23-1/programmers/큰수만들기.py
+++ 23-1/programmers/큰수만들기.py
@@ -1,1 +1,18 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/42883
+
+def solution(number, k):
+    arr = []
+    for i in number:
+        if len(arr) == 0:
+            arr.append(i)
+            continue
+        if k>0:
+            while arr[-1]<i:
+                arr.pop()
+                k-=1
+                if len(arr)==0 or k<1:
+                    break
+        arr.append(i)
+    #print("".join(arr))
+    arr  = arr[:-k] if k>0 else arr
+    return "".join(arr)
