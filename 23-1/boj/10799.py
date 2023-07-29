7b583a4645977566e620fbe51310f5e366e80269
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2023-07-30 02:33:37 +0900

    [boj] 10799 solved

--- 23-1/boj/10799.py
+++ 23-1/boj/10799.py
@@ -2,33 +2,16 @@ import sys
 input = sys.stdin.readline
 arr = list(input().strip())
 stack=[]
-l = [] # 레이저
-s = []
+answer = 0
 
-for idx, i in enumerate(arr):
-    if i == ")":
-        if stack:
-            temp = stack.pop()
-            if (idx-temp)==1: # 레이저인 경우
-                l.append([temp, idx])
-            else:
-                s.append([temp, idx]) # 스틱인 경우 
-    else:
+for idx in range(len(arr)):
+    if arr[idx]==")":
+        stack.pop()
+        if arr[idx-1] == "(":
+            answer+=len(stack)
+        else:
+            answer+=1
+    else: # arr[idx] == "(":
         stack.append(idx)
-            
-
-#print(l)
-#print(s)
-count = 0
-            
-for s_s, s_e in s:
-    for l_s, l_e in l:
-        if s_s<l_s and l_s<s_e: 
-            #print(l_s, l_e)
-            #print(s_s,s_e)
-            #print("=========")
-            count+=1
-    count+=1
-    #print(count)
-
-print(count)
+        
+print(answer)
