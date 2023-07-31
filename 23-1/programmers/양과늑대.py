8370e9ac5bbe406dbcd6883fa254c161c1cf1e4d
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2023-07-31 16:40:21 +0900

    [proj] 양과 늑대: using dfs & 완전 탐색

--- 23-1/programmers/양과늑대.py
+++ 23-1/programmers/양과늑대.py
@@ -1,26 +1,40 @@
-def solution(info, edges):
-    visited = [False]*len(info)
-    wolf = sheep = 0
-    answer = []
-    
-    def dfs(sheep, wolf):
-        if sheep>wolf:
-            answer.append(sheep)
-        else:
-            return
-        
-        for p, c in edges:
-            if visited[p] and not(visited[c]):
-                visited[c] = True
-                if info[c] == 0: # 0: 양
-                    dfs(sheep+1, wolf)
-                else: # 1: 늑대 
-                    dfs(sheep, wolf+1)
-                visited[c] = False 
-    
-    # 루트 노드 (양) 방문
-    visited[0] = True
-    dfs(sheep+1, wolf)
-    answer = max(answer)
+SHEEP = 0
+WOLF = 1
 
+def create_nextNodes(idx, node, nodes, edge_list):
+    # 노드 리스트에서 현재 인덱스의 노드 제외 + 현재 노드의 자식 노드 
+    #print("idx",idx)
+    #print("node", node)
+    #print(edge_list[node])
+    return nodes[:idx] +  nodes[idx+1:]+edge_list[node]
+
+def create_parent2child(info, edges):
+    edge_list = [[] for _ in range(len(info))]
+    for parent, child in edges:
+        edge_list[parent].append(child)
+    return edge_list
+
+def search(nodes, edge_list, info, sheep, wolf):
+    if not nodes: # 더이상 탐색 없음
+        return sheep
+    max_sheep = sheep
+    for idx, node in enumerate(nodes):
+        new_next_nodes = create_nextNodes(idx, node, nodes, edge_list)
+        if info[node] == SHEEP:
+            max_sheep =max(search(new_next_nodes, edge_list, info, sheep+1, wolf), max_sheep)
+        elif sheep>wolf+1:
+            max_sheep =max(search(new_next_nodes, edge_list, info, sheep, wolf+1),max_sheep)
+    return max_sheep
+        
+
+def solution(info, edges):
+    edge_list = create_parent2child(info, edges)
+    #print(len(edge_list))
+    answer = search([0], edge_list, info, 0, 0)
     return answer
+
+#info  =[0,0,1,1,1,0,1,0,1,0,1,1]
+#edges=[[0,1],[1,2],[1,4],[0,8],[8,7],[9,10],[9,11],[4,3],[6,5],[4,6],[8,9]]
+
+#solution(info, edges)
+    
