e222f3e95b33f3b9a01ca3933bfa71f9e2305d2f
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2023-08-01 15:36:47 +0900

    [level 2] Title: 양궁대회, Time: 5.20 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/92342. 양궁대회/README.md
+++ 프로그래머스/lv2/92342. 양궁대회/README.md
@@ -1,10 +1,10 @@
 # [level 2] 양궁대회 - 92342 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92342?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92342) 
 
 ### 성능 요약
 
-메모리: 10.4 MB, 시간: 2.59 ms
+메모리: 10.2 MB, 시간: 5.20 ms
 
 ### 구분
 
--- 프로그래머스/lv2/92342. 양궁대회/양궁대회.py
+++ 프로그래머스/lv2/92342. 양궁대회/양궁대회.py
@@ -1,45 +1,43 @@
-
 def solution(n, info):
     score_diff, answer = search(0, n, [0]*11, info)
-    if score_diff <=0:
+    if score_diff<=0:
         return [-1]
     return answer
     
-def calculate_score_diff(rinfo, ainfo):
-    rscore = ascore = 0
-    for idx in range(len(rinfo)):
-        if rinfo[idx] == ainfo[idx] == 0:
+def cal_score(rinfo, ainfo):
+    r_score = a_score = 0
+    for i in range(len(rinfo)):
+        if ainfo[i]==rinfo[i]==0:
             continue
-        if rinfo[idx]>ainfo[idx]:
-            rscore+=(10-idx)
+        elif ainfo[i]>=rinfo[i]:
+            a_score+=(10-i)
         else:
-            ascore+=(10-idx)
-    return rscore - ascore
-
-def better(info1, info2):
-    r_info1 = list(reversed(info1))
-    r_info2 = list(reversed(info2))
-    for idx in range(len(info1)):
-        if r_info1[idx]>r_info2[idx]:
-            return True
-        elif r_info1[idx]<r_info2[idx]:
-            return False
-    return False
-
-
-def search(idx, n, rinfo, ainfo):
-    if idx == 11 or n == 0:
-        rinfo[-1] = n
-        diff = calculate_score_diff(rinfo, ainfo)
-        return diff, rinfo[:]
+            r_score+=(10-i)
+            
+    return r_score-a_score
     
-    diff1, rinfo1 = search(idx+1, n, rinfo, ainfo)
+def better(info2, info1):
+    r_info2 = list(reversed(info2))
+    r_info1 = list(reversed(info1))
+    
+    for i in range(len(r_info2)):
+        if r_info1[i]>r_info2[i]:return False
+        
+        elif r_info2[i]>r_info1[i]: return True
+        
+    return False
+    
+def search(idx, n, rinfo, ainfo):
+    if idx==11 or n==0:
+        rinfo[-1] = n
+        diff = cal_score(rinfo, ainfo)
+        return diff, rinfo[:]
+    diff1, info1 = search(idx+1, n, rinfo, ainfo)
     if n > ainfo[idx]:
         rinfo[idx] = ainfo[idx]+1
-        diff2, rinfo2 = search(idx+1, n - rinfo[idx], rinfo, ainfo)
+        diff2, info2 = search(idx+1, n - rinfo[idx], rinfo, ainfo)
         rinfo[idx] = 0
-        if diff2 > diff1 or (diff2 == diff1 and better(rinfo2, rinfo1)):
-            return diff2, rinfo2
-    return diff1, rinfo1
-
-
+        if ((diff2 == diff1) and better(info2, info1)) or diff2>diff1:
+            return diff2, info2
+    return diff1, info1
+    
