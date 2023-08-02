f352e74132e75d764f31d19355a24728d165b9fb
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2023-08-02 11:41:18 +0900

    [level 2] Title: 양궁대회, Time: 2.96 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/92342. 양궁대회/양궁대회.py
+++ 프로그래머스/lv2/92342. 양궁대회/양궁대회.py
@@ -1,49 +1,51 @@
-# 라이언 점수표, 어피치 점수표 
-def cal_score(rinfo, ainfo):
-    r_score = a_score = 0
-    for i in range(len(rinfo)):
-        if ainfo[i]==rinfo[i]==0:
-            continue
-        elif ainfo[i]>=rinfo[i]:
-            a_score+=(10-i)
-        else:
-            r_score+=(10-i)
-    return r_score - a_score
 
-# 작은 점수 순으로 확인해서 더 적게 쏜 순간 False
+def cal_score(rinfo, ainfo):
+    rscore = 0
+    ascore = 0
+    for idx in range(len(rinfo)):
+        if rinfo[idx] == ainfo[idx] == 0:
+            continue
+        elif rinfo[idx]>ainfo[idx]:
+            rscore+=(10-idx)
+        else:
+            ascore+=(10-idx)
+        
+    return rscore - ascore
+
 def better(info2, info1):
     r_info2 = list(reversed(info2))
     r_info1 = list(reversed(info1))
-    for idx in range(len(info2)):
-        if r_info1[idx]>r_info2[idx]:
-            return False
-        elif r_info2[idx]>r_info1[idx]:
+    for idx in range(len(r_info2)):
+        if r_info2[idx]>r_info1[idx]:
             return True
-            
+        elif r_info1[idx]>r_info2[idx]:
+            return False
     return False
-    
 
-# 현재 탐색 점수 인덱스, 남은 화살수, 라이언 화살표, 어피치 화살표
 def search(idx, n, rinfo, ainfo):
-    if idx == 11 or n==0:
-        rinfo[-1]=n
+    if idx==11 or n == 0:
+        rinfo[-1] = n
         diff = cal_score(rinfo, ainfo)
         return diff, rinfo[:]
-    # 안쏘고 넘어가는 경우
-    diff1,info1 = search(idx+1, n, rinfo, ainfo)
     
-    # 쏘고 넘어가는 경우 
-    if n>ainfo[idx]:
+    # 안 쏘고 넘기는 경우
+    diff1, info1 = search(idx+1, n, rinfo, ainfo)
+    
+    # 화살 쏘는 경우 (복구 해야함)
+    if n > ainfo[idx]:
         rinfo[idx] = ainfo[idx]+1
-        diff2, info2 = search(idx+1, n -rinfo[idx], rinfo, ainfo)
+        diff2, info2 = search(idx+1, n - rinfo[idx], rinfo, ainfo)
         rinfo[idx] = 0
-        if diff2>diff1 or ((diff2 == diff1) and (better(info2, info1))):
+        if (diff2>diff1) or ((diff2 == diff1) and (better(info2, info1))):
             return diff2, info2
+            
     return diff1, info1
+    
 
 
 def solution(n, info):
-    diff, answer = search(0, n,[0]*11,info)
-    if diff<=0: return [-1]
+    diff, answer = search(0, n, [0]*11, info)
+    if diff<=0:
+        return [-1]
     return answer
 
