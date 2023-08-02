412c38956be8d5e00d4ca2766c55a2fec52e735f
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2023-08-02 11:53:12 +0900

    [level 3] Title: 양과 늑대, Time: 0.75 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv3/92343. 양과 늑대/양과 늑대.py
+++ 프로그래머스/lv3/92343. 양과 늑대/양과 늑대.py
@@ -1,30 +1,29 @@
-
-def create_parent2child(info, edges):
-    
+def create_edge_list(info, edges):
     edge_list = [[] for _ in range(len(info))]
     for parent, child in edges:
         edge_list[parent].append(child)
     return edge_list
     
-def create_nextNodes(idx,node, nodes, edge_list):
+def create_node_list(idx,node, nodes, edge_list):
     return nodes[:idx]+nodes[idx+1:]+edge_list[node]
-
+    
 def search(nodes, edge_list, info, sheep, wolf):
-    if not(nodes):
+    if not(nodes): # 더이상 노드 없는 경우
         return sheep
+    
     max_sheep = sheep
     for idx, node in enumerate(nodes):
-        next_nodes = create_nextNodes(idx,node, nodes, edge_list)
-        #print(next_nodes)
-        if info[node] == 0:
-            max_sheep = max(search(next_nodes, edge_list, info, sheep+1, wolf),max_sheep)
+        new_nodes = create_node_list(idx ,node, nodes ,edge_list)
+        #print(new_nodes)
+        if info[node]==0:
+            max_sheep =  max(max_sheep,search(new_nodes, edge_list, info, sheep+1, wolf))
         elif info[node]==1 and sheep>wolf+1:
-            max_sheep = max(search(next_nodes, edge_list, info, sheep, wolf+1), max_sheep)
+            max_sheep = max(max_sheep, search(new_nodes, edge_list, info, sheep, wolf+1))
     return max_sheep
-            
+
+
 def solution(info, edges):
-    edge_list = create_parent2child(info, edges)
-    answer = search([0], edge_list, info, 0, 0)
+    edge_list = create_edge_list(info, edges)
+    answer = search([0], edge_list,info,  0, 0)
     return answer
-    
 
