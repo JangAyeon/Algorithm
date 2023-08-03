137a66ee6807cebfafe4afc189606407f4c9684b
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2023-08-03 13:35:55 +0900

    [Silver I] Title: 숨바꼭질, Time: 96 ms, Memory: 35356 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -0,0 +1,26 @@
+## https://www.acmicpc.net/problem/1697
+
+import sys
+from collections import deque
+
+a,b = map(int, sys.stdin.readline().split())
+MAX = 100000
+visited = [0]*(MAX+1)
+
+def bfs(a):
+    que = deque()
+    que.append(a)
+    while que:
+        x = que.popleft()
+        if x == b:
+            print(visited[x])
+            return
+        for dx in (x-1,x+1,x*2):
+            if 0<= dx <=MAX and not visited[dx]:
+                que.append(dx)
+                visited[dx] = visited[x]+1
+
+
+
+
+bfs(a)
