c364802ee60f8fa2b450af705cb364a9760fcef4
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2023-08-04 15:31:36 +0900

    [boj] 10816: solved without dictionary counter

--- 23-1/boj/10816.py
+++ 23-1/boj/10816.py
@@ -10,9 +10,37 @@ def binary_search(arr, target, start, end):
         
     mid = (start + end)//2
     
-    # 찾은 경우 갯수 반환
+    # 찾은 경우 해당 mid 앞뒤로 중복 원소 있는지 확인 필요
     if arr[mid] == target:
-        return count[target]
+        i, j = 1, 1
+        
+        # mid에서 한 칸씩 앞으로
+        while mid - i>=start:
+            #print("---전진확인---")
+            #print(target,i,j)
+            #print(arr[mid-i], arr[mid])
+            #print("---전진확인 끝---")
+            
+            if arr[mid-i]!=arr[mid]: # 앞에 다른 수임 더이상 확인 필요 X
+                #print("전진 끝")
+                break
+            else: 
+                i+=1
+        # mid에서 한 칸씩 뒤로
+        while mid+j<=end:
+            #print("---후진확인---")
+            #print(target,i,j)
+            #print(arr[mid+j], arr[mid])
+            #print("---후진확인 끝---")
+            
+            if arr[mid+j]!=arr[mid]: # 뒤에 다른 수임 더이상 확인 필요 X
+                #print("후진 끝")
+                break
+            else:
+                j+=1
+        
+        return i+j-1
+            
         
     # 중간점의 값보다 찾는 값이 작은 경우 왼쪽 확인 필요
     elif arr[mid]>target: # 줄여야함
@@ -21,18 +49,15 @@ def binary_search(arr, target, start, end):
     else: # 키워야 함
         return binary_search(arr, target,mid+1, end)
 
-# 숫자 갯수 사전 활용 
-count={}
-for i in arr:
-    if i in count:
-        count[i]+=1
-    else:
-        count[i]=1
 
-answer = []
-#print(count)
-for num in search:
-    print(binary_search(arr, num, 0, len(arr)-1), end = " ")
+n_dic = {}
+for target in search:
+    start = 0
+    end = len(arr) - 1
+    if target not in n_dic:
+        n_dic[target] = binary_search(arr, target, start, end)
+
+print(' '.join(str(n_dic[x]) if x in n_dic else '0' for x in search ))
     
 
         
