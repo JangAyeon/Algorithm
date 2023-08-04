49288c108c1694a72e330c21a8c1cfe4151fbe4b
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2023-08-04 14:24:23 +0900

    [boj] 1920 solved

--- 23-1/boj/1920.py
+++ 23-1/boj/1920.py
@@ -3,33 +3,20 @@ n = int(input())
 arr = sorted(list(map(int,input().split())))
 m = int(input())
 search = list(map(int,input().split()))
-s_search = sorted(search)
 
-start, end = 0, len(arr)-1
-answer = [0 for _ in range(len(search))]
 
-while s_search:
-    num = s_search.pop(0)
-    idx = search.index(num)
+for num in search:
     start, end = 0, len(arr)-1
+    flag  = False
     while start<=end:
         mid = (start+end)//2
-        #print("========")
-        #print(start, mid, end)
-        #print(arr[mid], num)
-        #print("========")
         if arr[mid] == num:
-            answer[idx]=1
-            #print("find")
+            print(1)
+            flag = True
             break
         elif arr[mid]>num: # 줄여야 함
             end = mid-1
         else:
             start = mid+1
-            
-    #print(num,idx, answer, search, s_search)
-
-#print(n,m,arr, search)
-
-for i in answer:
-    print(i)
+    if not(flag):
+        print(0)
