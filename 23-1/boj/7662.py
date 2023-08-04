86a06aef10b4507d27d6f9645e795642c6de8779
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2023-08-04 12:41:52 +0900

    [boj] 7662: time over

--- 23-1/boj/7662.py
+++ 23-1/boj/7662.py
@@ -1,23 +1,28 @@
 import sys
 input = sys.stdin.readline
 
-T  = int(input())
+from heapq import heappush, nsmallest, nlargest
+
+T = int(input())
 
 for _ in range(T):
     n = int(input())
-    arr = []
+    heap = []
     for _ in range(n):
         op, num = input().strip().split()
         if op == "I":
-            arr.append(int(num))
-        elif op=="D" and arr:
-            if int(num) == -1:
-                arr.remove(min(arr))
-            elif int(num) == 1:
-                arr.remove(max(arr))
-        
-    if arr:
-        print(max(arr)," ",min(arr))
-    else:
+            heappush(heap, int(num))
+        elif op == "D" and heap:
+            if int(num) == -1: # 최소값 삭제
+                value = nsmallest(1, heap)[0]
+                #print(value, heap)
+                heap.remove(value)
+            elif int(num) == 1: # 최대값 삭제
+                value = nlargest(1, heap)[0]
+                heap.remove(value)
+                
+                
+    if len(heap) == 0:
         print("EMPTY")
-        
+    else:
+        print(nlargest(1,heap)[0], " ",nsmallest(1,heap)[0])
