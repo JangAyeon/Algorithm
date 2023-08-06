5ac1b4c921339c8be869eb4eb48bd00520830ab6
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2023-08-06 09:42:28 +0900

    [level 2] Title: 문자열 압축, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.py
+++ 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.py
@@ -1,26 +1,42 @@
-
-
 def solution(s):
-    
-    # 가장 긴 길이인 문자열 전체 길이를 초기값으로 설정
-    answer = len(s)
-    
-    # 문자열 슬라이스 길이가 절반을 넘어가면 압축 불가능함
-    for unit in range(1, len(s)//2+1):
-        zip_len = 0
-        start = 0
-        num = 1
-        while start+unit <=len(s):
-            sub = s[start:start+unit]
-            while sub == s[start+unit*num: start+unit*(num+1)]:
-                num+=1
-            if num>1:
-                zip_len+=len(str(num))
-            zip_len+=len(sub)
-            start+=unit*num
-            num = 1
-        zip_len +=len(s)-start
-        answer = min(answer, zip_len)
+    min_len = len(s)
+    for unit in range(1, (len(s)//2)+1):
 
-    return answer
+        # 결과 문자열
+        result = ""    
 
+        # 첫 탐색 단위
+        previous = s[0:unit]
+
+        # 동일한 탐색 단위의 개수
+        count = 1
+
+        # 탐색 인덱스를 탐색 단위만큼 증가시키며, 이전 탐색 단위와 비교
+        for idx in range(unit, len(s), unit):
+
+            # 현재 탐색 단위를 슬라이싱
+            now = s[idx:idx+unit]
+
+            # 현재 탐색 단위가 이전 탐색 단위와 같다면
+            if (now == previous):
+                count += 1
+            # 현재 탐색 단위가 이전 탐색 단위와 다르다면
+            else:
+                if (count >= 2):
+                    result += str(count) + previous
+                else:
+                    result += previous
+                previous = now
+                count = 1
+
+        # 남아있는 문자열에 대해 처리
+        if (count >= 2):
+            result += str(count) + previous
+        else:
+            result += previous    
+
+        # 탐색 단위별로 압축 문자열의 길이를 비교해, 최소 압축 문자열의 길이를 저장
+        min_len = min(min_len, len(result))
+
+    return min_len
+    
