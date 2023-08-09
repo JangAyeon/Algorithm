f6b0461c2682a88ccc5784b28ce44beffd093295
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2023-08-09 15:33:35 +0900

    [boj] 1388 solved

--- 23-1/boj/1388.py
+++ 23-1/boj/1388.py
@@ -1,9 +1,10 @@
 import sys
+from collections import deque
 input = sys.stdin.readline
 
 row, col = map(int, input().split())
 graph = [list(input().strip()) for _ in range(row)]
-visited = [[False]*col for _ in range(row)]
+
 #print(row, col, graph)
 answer=0
 
@@ -13,33 +14,34 @@ def isAble(r,c):
         return False
     return True
 
-def dfs(r, c):
+def bfs(r, c):
     
-    # 방문처리(0 값으로 교체)를 이곳 최상단에서 공통적으로 하는 경우 
-    # 탐색해야하는 칸의 -(좌우), |(상하) 여부를 확인하는 조건문에 걸리지 않음
+    que = deque()
+    que.append([r,c])
     
-    # - 일떄
-    if graph[r][c] == "-":
-        graph[r][c] = 0 # 방문처리
-        for dc in [1, -1]:  # 같은행: 양옆 확인
-            nc = c + dc  
-            if isAble(r, nc) and graph[r][nc]=="-": # 유효한 이동 영역 확인 -> - 좌우 탐색인지 확인
-                dfs(r, nc)
+    while que:
+        r,c =  que.popleft()
+        if graph[r][c]=="-": # 양 옆에 조사
+            graph[r][c] = 0 # 방문 처리
+            for dc in [-1,1]:
+                nc = c + dc
+                if isAble(r, nc) and graph[r][nc] == "-": # 그 다음 타일도 연결된 경우
+                    que.append([r,nc])
+        if graph[r][c] == "|": # 위 아래 조사
+            graph[r][c] = 0 # 방문 처리
+            for dr in [-1, 1]:
+                nr = r + dr
+                if isAble(nr,c) and graph[nr][c]=="|":
+                    que.append([nr, c])
     
-    # | 일 때
-    if graph[r][c]== "|":
-        graph[r][c] = 0
-        for dr in [-1, 1]:
-            nr = r +  dr
-            if isAble(nr,c) and graph[nr][c]=="|": # 유효한 이동 영역 확인 -> - 상하 탐색인지 확인
-                dfs(nr,c)
+
 
 
 answer = 0
 for i in range(row):  
     for j in range(col):  
-        if graph[i][j]=="|" or graph[i][j]=="-":  # 다음노드가 - 나 |일 경우 (아직 방문하지 않은 경우)
-            dfs(i, j)  # 재귀함수 호출
+        if graph[i][j]!=0:  # 다음노드가 - 나 |일 경우 (아직 방문하지 않은 경우)
+            bfs(i, j)  # 재귀함수 호출
             answer += 1
 
 print(answer)
