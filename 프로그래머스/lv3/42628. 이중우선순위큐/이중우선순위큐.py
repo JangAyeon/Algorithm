73512cbe260338b4ac1df3a4ddeb45f458ac3610
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2023-08-09 11:50:19 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.py
+++ 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -1,26 +1,20 @@
-
-
-from heapq import heappush, nsmallest, nlargest
-
+from heapq import heappush, nlargest, nsmallest
 
 def solution(operations):
-    heap = []
+    heapq = []
     for operation in operations:
         op, num = operation.split()
-        if op == "I":
-            heappush(heap, int(num))
-        elif op == "D" and heap:
-            if int(num) == -1: # 최소값 삭제
-                value = nsmallest(1, heap)[0]
-                #print(value, heap)
-                heap.remove(value)
-            elif int(num) == 1: # 최대값 삭제
-                value = nlargest(1, heap)[0]
-                heap.remove(value)
-    if heap:
-        answer = [nlargest(1,heap)[0],nsmallest(1,heap)[0]]
+        #print(op, num, heapq)
+        if op =="I":
+            heappush(heapq, int(num))
+        if op == "D" and heapq:
+            if int(num)==-1: # 최소값 삭제
+                heapq.remove(nsmallest(1,heapq)[0])
+            elif int(num)==1: # 최대값 삭제
+                heapq.remove(nlargest(1,heapq)[0])
+                
+    if heapq:
+        answer = [nlargest(1,heapq)[0], nsmallest(1,heapq)[0]]
     else:
         answer = [0,0]
     return answer
-
-
