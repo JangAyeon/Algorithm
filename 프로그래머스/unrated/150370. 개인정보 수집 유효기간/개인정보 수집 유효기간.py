9616efd8b525b41290873b279637f3f62d716a59
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2023-08-10 12:50:39 +0900

    [unrated] Title: 개인정보 수집 유효기간, Time: 0.19 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
+++ 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
@@ -10,56 +10,32 @@ def createTermPeriod(terms):
         _terms[t] = int(m)
     return _terms
 
-def createLastDate(period, yyyy,mm,dd):
-    m = mm + period
-    y = yyyy 
-    d = dd
-    if m>12:
+def createDays(yyyy,mm,dd):
+    days = yyyy*(28*12) + mm*28+dd
+    return  days
 
-        if m%12==0:
-            y = y + (m//12)-1
-            m = m%12
-        else:
-            y+=m//12
-            m=m%12
-        if m==0:
-            m=12
-    if d==0:
-        d = 28
-        m-=1
-        if m==0:
-            m=12
-            
-    #print(y,m,d)
-    return  y, m,d
-
-def checkDate(yyyy,mm,dd, today):
-    y,m,d = list(map(int, today.split("."))) # 오늘
-    
-    # 보관이 불가능한 경우
-    if yyyy<y:
+def checkDate(keepsDays, today):
+    if keepsDays<=today: # 보관 불가능
         return False
-    if (yyyy==y and mm<m):
-        return False
-    if (yyyy==y and mm==m and dd<=d):
-        return False
-    return True
+    else:
+        return True
     
 
 def solution(today, terms, privacies):
     answer = []
     termPeriod = createTermPeriod(terms)
+    y,m,d = list(map(int, today.split("."))) # 오늘
     #print(termPeriod)
     for idx, info in enumerate(privacies):
         yyyy, mm, dd, t = getPersonalDateTerm(info)
-        _yyyy,_mm,_dd = createLastDate(termPeriod[t], yyyy,mm,dd)
-        #print("last date", _yyyy,_mm,_dd)
-        if not(checkDate(_yyyy,_mm,_dd, today)):
+        keepDays = createDays(yyyy,mm+termPeriod[t],dd)
+        today = createDays(y,m,d)
+        
+        if not(checkDate(keepDays, today)):
             answer.append(idx+1)
 
 
     return answer
-
 
 
 
