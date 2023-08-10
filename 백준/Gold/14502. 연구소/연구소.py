1bf2d12c5192736c6072330f1329258f9cdd674f
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2023-08-10 17:54:49 +0900

    [Gold IV] Title: 연구소, Time: 2508 ms, Memory: 34264 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -5,55 +5,64 @@ input = sys.stdin.readline
 
 row, col = map(int, input().split())
 graph = [list(map(int, input().split())) for _ in range(row)]
+
 # 0은 빈 칸, 1은 벽, 2는 바이러스
 dr = [-1,1,0,0]
 dc = [0,0,-1,1]
 answer = 0
-# 어디에 벽을 세울 수 있을지 모르기 때문에
-# 모든 경우의 수 수행
-# 백트레킹으로 벽 세우고 벽 지우는 방식 반복
 
-def make_wall(count):
-    if count == 3:
-        bfs()
+# virusList
+virusList = []
+
+# 깊이 우선 탐색으로 각 바이러스 사방에 퍼지게
+def spread(r,c, temp):
+    for idx in range(4):
+        nr = r + dr[idx]
+        nc = c + dc[idx]
+        
+        if 0<=nr<row and 0<=nc<col:
+            if temp[nr][nc] == 0: # 빈칸인 경우
+                temp[nr][nc] = 2
+                spread(nr,nc, temp)
+                
+                
+# 현재 맵에서 안전 영역 크기 계산
+def get_size(temp):
+    size = 0
+    for i in range(row):
+        for j in range(col):
+            if temp[i][j] == 0:
+                size+=1
+    #print(size)
+    return size
+    
+def dfs(start,count):
+    global answer
+    
+    if count==3:
+        # 그래프 복사
+        temp  = deepcopy(graph)
+        
+        # 바이러스 퍼트리기 
+        for i in range(len(virusList)):
+            r,c = virusList[i]
+            spread(r,c, temp)
+
+        answer = max(answer, get_size(temp))
         return
     
-    for i in range(row):
-        for j in range(col):
-            # 빈칸인 경우 벽 세우기
-            if graph[i][j] == 0:
-                graph[i][j] = 1
-                make_wall(count+1)
-                graph[i][j]=0
-
-def bfs():
-    que = deque()
-    temp_graph = deepcopy(graph)
-    for i in range(row):
-        for j in range(col):
-            if temp_graph[i][j]==2:
-                que.append([i,j])
-    
-    while que:
-        r,c = que.popleft()
-        for idx in range(4):
-            nr,nc = r +dr[idx], c + dc[idx]
-            if not(0<=nr<row and 0<=nc<col):
-                continue
-            if temp_graph[nr][nc] ==0:
-                temp_graph[nr][nc] = 2
-                que.append([nr, nc])
-                
-    global answer
-    #print("============")
-    #print(graph)
-    cnt = 0
-    
-    for i in range(row):
-        for j in range(col):
-            if temp_graph[i][j]==0:
-                cnt+=1
-    answer = max(answer, cnt)
+    for i in range(start, row*col):
+        x = int(i/col)
+        y = int(i%col)
+        if graph[x][y] == 0:
+            graph[x][y] = 1
+            dfs(i+1,count+1)
+            graph[x][y] = 0
             
-make_wall(0)    
+for i in range(row):
+    for j in range(col):
+        if graph[i][j] == 2:
+            virusList.append([i,j])
+
+dfs(0,0)
 print(answer)
