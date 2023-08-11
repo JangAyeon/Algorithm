11e53a44e818b287973b91313e14618f7a262cca
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2023-08-11 14:26:35 +0900

    [Silver III] Title: 계단 오르기, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+arr = [int(input()) for _ in range(n)]
+arr.insert(0,0)
+
+ans  = [0 for _ in range(n+1)]
+
+
+
+if n==1:
+    notStepped = 0
+else:
+    ans[1], ans[2]=arr[1], arr[2]
+    if n>2:
+        ans[3]= arr[3]
+        for i in range(4, n+1):
+            ans[i] = min(ans[i-2], ans[i-3])+arr[i]
+    notStepped = min(ans[n-1],ans[n-2])
+print(sum(arr)- notStepped)
