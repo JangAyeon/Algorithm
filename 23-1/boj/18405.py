ed0ce04aab3163021f970fe8ab7ff93aca7f91ef
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2023-08-11 16:24:06 +0900

    [boj] 18405 clean code

--- 23-1/boj/18405.py
+++ 23-1/boj/18405.py
@@ -1,52 +1,40 @@
-import sys
 from collections import deque
-from copy import deepcopy
-input = sys.stdin.readline
 
-# n: 그래프 row, k: 바이러스 종류 갯수
-n,k=map(int, input().split())
+n,k = map(int, input().split())
 graph = [list(map(int, input().split())) for _ in range(n)]
-s,x,y = map(int, input().split())
+target_s, target_x, target_y = map(int, input().split())
 
 dr = [-1,1,0,0]
 dc = [0,0,-1,1]
 
-virus = []
-que = deque()
-t_que = deque()
+# 바이러스 최초 위치 - 우선 순위 고려 X
+data = []
+
 
 for i in range(n):
     for j in range(n):
         if graph[i][j]!=0:
-            virus.append([i,j, graph[i][j]])
-            
-virus.sort(key = lambda x : x[2])
+            # 바이러스 종류, 현재 시간, r, c
+            data.append((graph[i][j],0,i,j))
 
-for r,c,v in virus:
-    que.append([r,c])
-            
-def bfs(s, que):
-    count=1
-    while que:
-        if count>s:
-                return
-        r,c = que.popleft()
-        for idx in range(4):
-            nr, nc = r + dr[idx], c+ dc[idx]
-            if not(0<=nr<n and 0<=nc<n):
-                continue
-            if graph[nr][nc]==0:
-                graph[nr][nc] = graph[r][c]
-                t_que.append([nr, nc])
-        #print("##",count,"##")
-        #for i in graph:
-        #    print(i)       
-        #print("####")
-        if len(que)==0:
-            count+=1
-            que = deepcopy(t_que)
-            t_que.clear()
+# 바이러스 종류 기준 오름 차순 정렬 -  우선 순위 맞추기 진행            
+data.sort()
+que = deque(data)
+
+# bfs
+while que:
+    v, s, r, c =que.popleft()
+    if s == target_s:
+        break
+    
+    for idx in range(4):
+        nr, nc = r + dr[idx], c + dc[idx]
+        if not(0<=nr<n and 0<=nc<n):
+            continue
+        if graph[nr][nc]==0:
+            graph[nr][nc] = v #graph[r][c]와 동일
+            que.append((graph[nr][nc], s+1, nr, nc))
             
             
-bfs(s, que)
-print(graph[x-1][y-1])
+print(graph[target_x-1][target_y-1])
+            
