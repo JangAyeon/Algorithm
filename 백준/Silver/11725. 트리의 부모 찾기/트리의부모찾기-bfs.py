5903912df23fd3ddd4a3596bf7874b41d464780e
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2023-08-12 14:57:44 +0900

    Create 트리의부모찾기-bfs.py

---
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의부모찾기-bfs.py
@@ -0,0 +1,25 @@
+import sys
+input = sys.stdin.readline
+from collections import deque
+
+n = int(input())
+parent = [0]*(n+1)
+graph=[[] for _ in range(n+1)]
+
+for _ in range(n-1):
+    a, b= map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+    
+def bfs(s):
+    que = deque()
+    que.append(s)
+    while que:
+        node = que.popleft()
+        for child in graph[node]:
+            if parent[child]==0:
+                parent[child] = node
+                que.append(child)
+    
+bfs(1)    
+print(*parent[2:], sep="\n")
