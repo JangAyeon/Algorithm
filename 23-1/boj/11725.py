36cbb4dd3253821321f31fd0b50009fb57cab571
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2023-08-12 15:00:51 +0900

    [boj] 11725 : dfs solved

---
+++ 23-1/boj/11725.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10**9)
+
+n = int(input())
+parent = [0]*(n+1)
+graph=[[] for _ in range(n+1)]
+
+for _ in range(n-1):
+    a, b= map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+    
+def dfs(node):
+    for child in graph[node]:
+        if parent[child]==0:
+            parent[child] = node
+            dfs(child)
+    
+dfs(1)    
+print(*parent[2:], sep="\n")
