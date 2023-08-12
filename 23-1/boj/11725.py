90f8cbd5974af575096676168b931f83bdd8318e
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2023-08-12 15:01:30 +0900

    [boj] 11725 : bfs solved

--- 23-1/boj/11725.py
+++ 23-1/boj/11725.py
@@ -1,21 +1,27 @@
 import sys
 input = sys.stdin.readline
-sys.setrecursionlimit(10**9)
+from collections import deque
 
 n = int(input())
-parent = [0]*(n+1)
-graph=[[] for _ in range(n+1)]
+graph = [[] for _ in range(n+1)]
+parent=[0]*(n+1)
+
 
 for _ in range(n-1):
-    a, b= map(int, input().split())
+    a,b = map(int, input().split())
     graph[a].append(b)
     graph[b].append(a)
-    
-def dfs(node):
-    for child in graph[node]:
-        if parent[child]==0:
-            parent[child] = node
-            dfs(child)
-    
-dfs(1)    
+
+def bfs(s):
+    que = deque()
+    que.append(s)
+    while que:
+        node = que.popleft()
+        for child in graph[node]:
+            if parent[child]==0:
+                parent[child]=node
+                que.append(child)
+
+
+bfs(1)
 print(*parent[2:], sep="\n")
