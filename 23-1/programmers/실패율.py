c822613ce02325ccf8609e85c3b5bc801bd2c37f
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2023-08-18 16:07:14 +0900

    [proj] 실패율 clean code

--- 23-1/programmers/실패율.py
+++ 23-1/programmers/실패율.py
@@ -1,24 +1,20 @@
 def solution(N, stages):
 
     peoples = len(stages)
-    stages.sort()
-    fail = [ 0 for _ in range(N+1)]
-    for idx in stages:
-        if idx==N+1:
-            continue
-        fail[idx]+=1
-    stage_rate = []
-    fail = fail[1:]
-    for idx, count in enumerate(fail):
-        fail_players = fail[idx]
-        stage_players = peoples-sum(fail[:idx])
-        if stage_players == 0:
-            rate = 0
+    users = [ 0 for _ in range(N+1)]
+    fails = []
+    for stage in stages:
+        users[stage-1] +=1
+        
+    for i in range(N):
+        if users[i]==0:
+            fails.append([0, i+1])
         else:
-            rate = fail_players/stage_players
-        stage_rate.append([rate, idx+1])
-    stage_rate.sort(key=lambda x : (-x[0] ))
+            fails.append([users[i]/peoples, i+1])
+            peoples-=users[i]
+            
+    fails.sort(key=lambda x : (-x[0] ))
     answer = []
-    for rate, num in stage_rate:
+    for rate, num in fails:
         answer.append(num)
     return answer
