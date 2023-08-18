c95e6c6ea339f37f811bd60da35b7b50f7251b9c
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2023-08-18 15:13:44 +0900

    [boj] 18310 clean code

--- 23-1/boj/18310.py
+++ 23-1/boj/18310.py
@@ -4,21 +4,4 @@ input = sys.stdin.readline
 n= int(input())
 arr = sorted(list(map(int, input().split())))
 
-def calc_distance(idx):
-    loc = arr[idx]
-    distance = 0
-    for house in arr:
-        distance+=abs(loc - house)
-    return distance, loc
-
-## 짝수인 경우
-if len(arr)%2==0:
-    answer = min(calc_distance(len(arr)//2), calc_distance(len(arr)//2-1))[1]
-
-## 홀수인 경우  
-else:
-    answer = arr[len(arr)//2]
-    
-    
-print(answer)
-
+print(arr[(n-1)//2])
