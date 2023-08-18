56d2f9b2574dfee686dbc7433d4ac9b4887a301f
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2023-08-18 15:11:31 +0900

    [boj] 18310 solved

--- 23-1/boj/18310.py
+++ 23-1/boj/18310.py
@@ -3,15 +3,22 @@ input = sys.stdin.readline
 
 n= int(input())
 arr = sorted(list(map(int, input().split())))
-distance = 1e9
-answer = 0
 
-for idx, loc in enumerate(arr):
-    temp = 0
+def calc_distance(idx):
+    loc = arr[idx]
+    distance = 0
     for house in arr:
-        temp += abs(loc-house)
-        #print(loc, house, temp)
-    if distance>temp:
-        distance = temp
-        answer = loc
+        distance+=abs(loc - house)
+    return distance, loc
+
+## 짝수인 경우
+if len(arr)%2==0:
+    answer = min(calc_distance(len(arr)//2), calc_distance(len(arr)//2-1))[1]
+
+## 홀수인 경우  
+else:
+    answer = arr[len(arr)//2]
+    
+    
 print(answer)
+
