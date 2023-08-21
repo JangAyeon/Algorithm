898e947acd43d7b924f1ea33a9103cbedcbfd3e3
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2023-08-21 13:17:08 +0900

    [boj] 15663 back-Tracking

--- 23-1/boj/15663.py
+++ 23-1/boj/15663.py
@@ -3,8 +3,28 @@ from itertools import permutations
 n,m=map(int, input().split())
 
 arr = sorted(list(map(int, input().split())))
+visited = [False]*n
+answer = []
 
-answer = sorted(list(set(permutations(arr,m))))
+
+def dfs(idx, lst):
+    
+    if idx==m:
+        answer.append(lst)
+        return
+    prev = 0
+    for j in range(n):
+        if not (visited[j]) and prev!=arr[j]:
+            visited[j]=True
+            prev = arr[j]
+            dfs(idx+1, lst+[arr[j]])
+            visited[j]=False
+            
+    
+    
+
+dfs(0, [])
+answer.sort()
 
 for i in answer:
     print(*i)
