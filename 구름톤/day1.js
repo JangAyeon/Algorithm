1e0bb54900cefa54abc456e5ffbc84b2e6729dbe
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2023-08-21 12:23:34 +0900

    [구름톤] day 1 JS

---
+++ 구름톤/day1.js
@@ -0,0 +1,30 @@
+const readline = require("readline");
+let rl = readline.createInterface({
+  input:process.stdin,
+  output:process.stdout,
+})
+
+// 입력되는 모든 데이터를 가지고 있을 input 리스트
+let input = [];
+
+// readline 모듈의 on 메소드 사용해 입력 받음 
+rl.on("line", (line)=>{
+  // 입력되는 문자열의 공백 단위로 나누고 숫자형 변환해 한개씩 리스트 저장
+  input.push(line.split(" ").map(Number));
+  // line 이벤트는 rl.close() 만나기 전까지 실행됨
+  // 입력 받는 기준은 ENTER(줄바꿈)임
+  // 문제에서 들어오는 입력은 1줄임 
+  if(input.length==1){
+    // 입력 종료
+    // line 이벤트 종료됨 
+    rl.close(); // close 이벤트 실행됨
+  }
+})
+
+// close 이벤트 핸들러 코드
+rl.on("close",()=>{
+  const [W, R] =input[0];
+  const answer = Math.trunc(W*(1+R/30));
+  console.log(answer);
+  rl.close();
+})
