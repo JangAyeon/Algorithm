b05c29cddd142b2e4d8ba81ef4b8b07f1e04d3fc
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2023-08-28 15:03:27 +0900

    [boj] 2580 solved

--- 23-1/boj/2580.py
+++ 23-1/boj/2580.py
@@ -1,46 +1,44 @@
 import sys
 input = sys.stdin.readline
 
-arr = [list(map(int, input().split())) for _ in range(9)]
-zero = [(x,y) for x in range(9) for y in range(9) if arr[x][y]==0]
-#print(arr, zero)
+graph = [list(map(int, input().split())) for _ in range(9)]
+zeros = [[row, col] for col in range(9) for row in range(9) if graph[row][col]==0]
 
-def check_x(i,x):
-    for y in range(9):
-        if arr[x][y]==i:
+def check_row(i, col):
+    for row in range(9):
+        if graph[row][col]==i:
             return False
     return True
-
-def check_y(i,y):
-    for x in range(9):
-        if arr[x][y] == i:
+    
+def check_col(i, row):
+    for col in range(9):
+        if graph[row][col]==i:
             return False
     return True
-
-
-def check_rect(i,x,y):
-    x = x//3*3
-    y = y//3*3
-    for dx in range(3):
-        for dy in range(3):
-            if  arr[x+dx][y+dy]==i:
+    
+def check_rect(i, row, col):
+    row = row//3*3
+    col = col//3*3
+    
+    for dr in range(3):
+        for dc in range(3):
+            if graph[row+dr][col+dc]==i:
                 return False
+                
     return True
-
-def dfs(cnt):
-    if cnt == len(zero):
-        for row in arr:
+    
+def dfs(count):
+    if count==len(zeros):
+        for row in graph:
             print(*row)
         exit()
-    x = zero[cnt][0]
-    y = zero[cnt][1]
+        
+    row, col = zeros[count]
+    
     for i in range(1,10):
-        #조건에 맞는 경우
-        if check_x(i,x) and check_y(i,y) and check_rect(i,x,y):
-            #print("답")
-            arr[x][y]=i
-            dfs(cnt+1)
-            arr[x][y]=0
-
-
+        if check_row(i, col) and check_col(i, row) and check_rect(i, row, col):
+            graph[row][col]=i
+            dfs(count+1)
+            graph[row][col]=0
+            
 dfs(0)
