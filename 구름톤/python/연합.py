d1b6b074f0dcd1ecc9fc8f3832d9ddaf0e0eda01
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2023-09-05 16:11:48 +0900

    [구름톤] day 15 BFS 이용

--- 구름톤/python/연합.py
+++ 구름톤/python/연합.py
@@ -48,5 +48,33 @@ print(len(answer))
 
 
 
+## BFS 이용
+import sys
+input = sys.stdin.readline
+from collections import deque
+# 섬의 갯수, 다리갯수
+n,m = map(int, input().split())
+graph = [[] for _ in range(n+1)]
+visited = [0]*(n+1) # BFS를 위한 방문 배열
+result = 0
 
+
+for _ in range(m):
+	start, end = map(int, input().split())
+	graph[start].append(end) # 단방향으로 이동
 	
+for i in range(1, n+1):
+	if visited[i]:
+		continue
+	que = deque([i])
+	result+=1
+	visited[i]=True
+	while que:
+		node = que.popleft()
+		for nextNode in graph[node]:
+			# 두 섬은 서로 이동할 수 있어야 함
+			# 가는 간선과 오는 간선이 동시에 존재
+			if not visited[nextNode] and node in graph[nextNode]:
+				que.append(nextNode)
+				visited[nextNode]=True
+print(result)
