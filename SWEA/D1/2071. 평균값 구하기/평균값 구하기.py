e1248f9e725ec7b05543841f16e3208ef56dad7d
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2023-10-23 01:33:39 +0900

    [D1] Title: 평균값 구하기, Time: 95 ms, Memory: 41,676 KB -BaekjoonHub

---
+++ SWEA/D1/2071. 평균값 구하기/평균값 구하기.py
@@ -0,0 +1,10 @@
+T = int(input())
+ans = []
+# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+for test_case in range(1, T + 1):
+    arr = list(map(int, input().split()))
+    avg=round(sum(arr)/len(arr))
+    ans.append(avg)
+    
+for idx, value in enumerate(ans):
+    print("#"+str(idx+1),value)
