3e3e1e43deb4abd4d70a21c3cb65eb4113c47e85
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2023-11-12 11:31:51 +0900

    [Bronze V] Title: 사칙연산, Time: 128 ms, Memory: 9608 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.js
@@ -0,0 +1,11 @@
+let fs = require("fs");
+let line = fs.readFileSync("/dev/stdin").toString().split(" ");
+
+let num1 = parseInt(line[0])
+let num2 = parseInt(line[1])
+
+console.log(num1+num2)
+console.log(num1-num2)
+console.log(num1*num2)
+console.log(Math.floor(num1/num2))
+console.log(num1%num2)
