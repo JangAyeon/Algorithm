02ed5962772f05b8c2baa6ae4c0e365cd76e3c27
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2023-11-23 15:59:26 +0900

    [Gold II] Title: 문제집, Time: 228 ms, Memory: 40652 KB -BaekjoonHub

---
+++ 백준/Gold/1766. 문제집/문제집.py
@@ -0,0 +1,30 @@
+import sys
+input = sys.stdin.readline
+from heapq import heappush, heappop
+
+# 문제 수, 문제 정보 수
+n,m = map(int, input().split())
+
+indegree = [0 for _  in range(n+1)]
+graph = [[] for _ in range(n+1)]
+
+for _ in range(m):
+    a,b = map(int, input().split())
+    graph[a].append(b)
+    indegree[b]+=1
+heap = []
+
+for i in range(1, n+1):
+    if(indegree[i]==0):
+        heappush(heap,i)
+answer=[]
+while heap:
+    curr = heappop(heap)
+    answer.append(curr)
+    for next_ in graph[curr]:
+        indegree[next_]-=1
+        if(indegree[next_]==0):
+            heappush(heap, next_)
+            
+        
+print(*answer)
