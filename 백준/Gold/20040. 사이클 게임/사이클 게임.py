8e027b7ef75ca26110b7e3f31c0f71452b8fce3e
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2023-11-27 17:30:49 +0900

    [Gold IV] Title: 사이클 게임, Time: 788 ms, Memory: 50384 KB -BaekjoonHub

--- 백준/Gold/20040. 사이클 게임/사이클 게임.py
+++ 백준/Gold/20040. 사이클 게임/사이클 게임.py
@@ -1,35 +1,38 @@
 import sys
 input = sys.stdin.readline
 
+# 점의 갯수 N, 진행된 차례 M
+N, M = map(int, input().split())
+# 0 ~ n-1 고유 번호가 부여됨
+parent = list(range(N))
+
+
 def findParent(node, parent):
     if parent[node]!=node:
         parent[node] = findParent(parent[node], parent)
     return parent[node]
+    
+def findUnion(a,b, parent):
+    a = findParent( a,parent)
+    b = findParent( b, parent)
 
-def findUnion(parent, a, b):
-    a = findParent(a, parent)
-    b = findParent(b, parent)
-
+    # 더 작은 쪽으로 맞추기
     if a<b:
         parent[b] = a
     else:
         parent[a] = b
 
-# 점의 갯수, 진행된 차례 
-n,m = map(int, input().split())
-parent  = [i for i in range(n)] 
-flag = False
-
-for t in range(m):
-    a, b = map(int, input().split())
-    if findParent(a, parent)==findParent(b, parent):
+flag= False
+for t in range(1,M+1):
+    a,b = map(int, input().split())
+    # 싸이클 발생한 경우
+    if findParent(a, parent) == findParent(b, parent):
         flag = True
-        print(t+1)
-        break
-    if flag==False:
-        findUnion(parent, a,b)
-    else:
-        break
-if flag==False:
+        print(t)
+        exit()
+    # 싸이클 발생하지 않아 조상 할당
+    findUnion(a,b, parent)
+
+if not flag:
     print(0)
     
