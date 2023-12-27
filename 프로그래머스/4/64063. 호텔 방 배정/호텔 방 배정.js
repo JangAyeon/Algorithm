9a8e7e934f19b15f9ca6a0a1d805e69fda2cb2af
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2023-12-27 16:59:58 +0900

    [level 4] Title: 호텔 방 배정, Time: 424.74 ms, Memory: 96.1 MB -BaekjoonHub

---
+++ 프로그래머스/4/64063. 호텔 방 배정/호텔 방 배정.js
@@ -0,0 +1,17 @@
+function solution(k, room_number) {
+  const rooms = new Map();
+
+  const assignRooms = (num) => {
+    if (!rooms.has(num)) {
+      // 아직 배정받지 않은 방이라면, 다음 번호의 방을 담아준다.
+      rooms.set(num, num + 1);
+      return num;
+    }
+    // 이미 배정 받은 방이라면, 가리키고 있는 방이 배정 받았는지 확인한다.
+    const nearestRoom = assignRooms(rooms.get(num));
+    rooms.set(num, nearestRoom + 1);
+    return nearestRoom;
+  };
+
+  return room_number.map(assignRooms);
+}
