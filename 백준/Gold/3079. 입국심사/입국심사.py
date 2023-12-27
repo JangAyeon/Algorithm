264e86c573773bf4c64330bff890343c8267a3d0
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2023-12-27 23:13:02 +0900

    [Gold V] Title: 입국심사, Time: 1196 ms, Memory: 34972 KB -BaekjoonHub

--- 백준/Gold/3079. 입국심사/입국심사.py
+++ 백준/Gold/3079. 입국심사/입국심사.py
@@ -1,23 +1,25 @@
-from sys import stdin
+import sys
+input  = sys.stdin.readline
 
-n, m = map(int, stdin.readline().split())
-arr = [int(stdin.readline()) for _ in range(n)]
+n,m  = map(int, input().split())
+arr = [int(input()) for _ in range(n)]
 
-l, r = min(arr), max(arr) * m
+l, r = min(arr), max(arr)*m
 ans = r
 
-while l <= r:
+while l<=r:
     total = 0
-    mid = (l + r) // 2
+    mid = (l+r)//2
 
     for i in range(n):
-        total += mid // arr[i]
+        total+=mid//arr[i]
 
-    if total >= m:
-        r = mid -1
-        ans = min(mid, ans)
-
+    ## 모두 수용 가능한 경우
+    if total>=m:
+        r = mid-1
+        ans=min(mid, ans)
+    ## 부족해서 수용 불가능한 경우 
     else:
-        l = mid + 1
+        l = mid+1
 
 print(ans)
