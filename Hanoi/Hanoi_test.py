340a412d1fe6a0338878f6e5f075327a2ffa1546
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2024-02-15 09:56:59 +0900

    feat: refactor folder structer and solved 24.02.15

--- Hanoi/Hanoi_test.py
+++
@@ -1,7 +0,0 @@
-from Hanoi_recur import hanoi
-
-for i in range(1,6):
-    print("### 원판이 ", i,"개인 경우")
-    #원판 i개를 1반 기둥에서 3번 기둥으로 이동 (2번을 보조 기둥으로)
-    total_cnt=hanoi(i, 1,3,2) 
-    print("총 이동 횟수 : ",total_cnt )
