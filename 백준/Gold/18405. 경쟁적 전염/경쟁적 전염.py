5db0831a1ef9ec0be8b3584b5fd27c2aac4fffa1
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2024-02-17 17:07:11 +0900

    [Gold V] Title: 경쟁적 전염, Time: 112 ms, Memory: 34344 KB -BaekjoonHub

--- 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.py
+++ 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.py
@@ -1,40 +1,49 @@
+## https://www.acmicpc.net/problem/18405
+
+import sys
+input = sys.stdin.readline
 from collections import deque
 
-n,k = map(int, input().split())
+graph = []
+n,k =  map(int, input().split())
 graph = [list(map(int, input().split())) for _ in range(n)]
-target_s, target_x, target_y = map(int, input().split())
+s,x,y = map(int, input().split())
+que = deque()
+virus = []
+## print(graph, s,x,y)
+
+
+
+    
+for r in range(len(graph)):
+    for c in range(len(graph[0])):
+        if (graph[r][c]):
+            virus.append([graph[r][c],r,c])
+virus.sort()
+
+for v,r,c in virus:
+    que.append([r,c,0])
 
 dr = [-1,1,0,0]
 dc = [0,0,-1,1]
 
-# 바이러스 최초 위치 - 우선 순위 고려 X
-data = []
 
-
-for i in range(n):
-    for j in range(n):
-        if graph[i][j]!=0:
-            # 바이러스 종류, 현재 시간, r, c
-            data.append((graph[i][j],0,i,j))
-
-# 바이러스 종류 기준 오름 차순 정렬 -  우선 순위 맞추기 진행            
-data.sort()
-que = deque(data)
-
-# bfs
-while que:
-    v, s, r, c =que.popleft()
-    if s == target_s:
-        break
-    
-    for idx in range(4):
-        nr, nc = r + dr[idx], c + dc[idx]
-        if not(0<=nr<n and 0<=nc<n):
+def bfs():
+    while que:
+        r,c, time = que.popleft()
+        
+        if time ==s:
             continue
-        if graph[nr][nc]==0:
-            graph[nr][nc] = v #graph[r][c]와 동일
-            que.append((graph[nr][nc], s+1, nr, nc))
-            
-            
-print(graph[target_x-1][target_y-1])
-            
+
+        for idx in range(4):
+            nr, nc = r + dr[idx], c + dc[idx]
+            if not(0<=nr<n) or not(0<=nc<n): ## 범위 벗어나는 경우
+                continue
+            if graph[nr][nc]==0:
+                graph[nr][nc]=graph[r][c]
+                que.append([nr, nc, time+1])
+
+
+
+bfs()            
+print(graph[x-1][y-1])
