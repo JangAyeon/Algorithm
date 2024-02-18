6729468b0a81358678bbbf70921af3463f1d69bc
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2024-02-19 01:33:25 +0900

    [Gold III] Title: 텀 프로젝트, Time: 2400 ms, Memory: 60828 KB -BaekjoonHub

--- 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.py
+++ 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.py
@@ -1,32 +1,31 @@
 import sys
-sys.setrecursionlimit(10**6) 
+sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
-
-
 
 def dfs(x):
     global result
     visited[x]=True
     cycle.append(x)
-    pair=arr[x]
+    pair = arr[x]
 
     if visited[pair]:
         if pair in cycle:
-            result+=cycle[cycle.index(pair):]
+            result +=cycle[cycle.index(pair):]
         return
     else:
         dfs(pair)
-            
-    
+
 T = int(input())
 
 for _ in range(T):
+    result = []
     n = int(input())
     arr = [0]+list(map(int, input().split()))
     visited = [False]*(n+1)
-    result = []
-    for idx in range(1, len(arr)):
+
+    for idx in range(1, n+1):
         if not(visited[idx]):
             cycle = []
             dfs(idx)
-    print(n-len(result))
+
+    print(n - len(result))
