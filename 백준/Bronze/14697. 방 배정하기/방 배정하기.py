63d032e63662e7b55c05c2ef2706423859e42374
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2024-04-08 10:16:52 +0900

    [Bronze II] Title: 방 배정하기, Time: 84 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/14697. 방 배정하기/방 배정하기.py
+++ 백준/Bronze/14697. 방 배정하기/방 배정하기.py
@@ -1,45 +1,13 @@
-import sys
-input = sys.stdin.readline
+a,b,c,student = map(int,input().split())
+flag = 0
+for i in range(student//a + 1):
+	for j in range(student//b + 1):
+		for k in range(student//c + 1):
+			if a*i + b*j + c*k == student:
+				flag = 1
+				break
 
-
-lst = list(map(int, input().split()))
-arr, n = lst[:-1], lst[-1]
-## print(arr, n)
-
-
-"""
-5 9 12 113
-ans: 1  
-
-2 3 4 13
-ans: 1
-
-1 2 3 300
-ans: 1
-
-3 6 9 112
-ans: 0
-"""
-
-
-def sol(num, d, visited):
-    ##print(num, d, visited)
-    if num==0:
-        print(1)
-        exit() 
-    elif sum(visited)==len(visited):
-        return
-    else:
-        visited[arr.index(d)]=1
-        for j in range(num//d+1):
-            for i in range(len(arr)):
-                if not(visited[i]):
-                    ## print(num-d*j,j, d,visited)
-                    visited[i]=1
-                    sol(num-d*j,arr[i],visited)
-                    visited[i]=0
-
-       
-for idx in range(len(arr)):
-    sol(n, arr[idx], [0]*(len(arr)))
-print(0)
+if flag == 1:
+	print(1)
+else:
+	print(0)
