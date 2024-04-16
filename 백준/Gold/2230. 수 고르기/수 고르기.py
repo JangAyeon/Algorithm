a710f2ad36121d5593bd9edd23a5c8ac7a211f1e
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2024-04-16 16:45:49 +0900

    [Gold V] Title: 수 고르기, Time: 192 ms, Memory: 35364 KB -BaekjoonHub

--- 백준/Gold/2230. 수 고르기/수 고르기.py
+++ 백준/Gold/2230. 수 고르기/수 고르기.py
@@ -7,13 +7,12 @@ for _ in range(n):
     arr.append(int(input()))
 
 arr.sort()
-
-
-s = 0
-e=0
 answer =max(arr)-min(arr)
-
-while (s <= e and e < len(arr)):
+s,e =0,0
+        
+while True:
+        if not(s <= e and e < len(arr)):
+            break
         diff =  arr[e]-arr[s]
         if diff==m:
             answer = m
@@ -27,48 +26,3 @@ while (s <= e and e < len(arr)):
 
 
 print(answer)
-"""
-3 3
-1
-5
-3
-ans:4
-"""
-
-"""
-5 3
-1
-2
-3
-4
-5
-ans: 3
-"""
-
-"""
-5 6
-1
-2
-3
-4
-11
-ans: 7
-"""
-
-"""
-2 2000000000
--1000000000
-1000000000
-
-"""
-
-"""
-7 4
-1
-8
-15
-16
-17
-18
-22
-"""
