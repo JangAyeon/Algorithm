414c80e56270e7e61bfe05e24852c9f903503658
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2024-04-17 13:42:28 +0900

    [Silver III] Title: 로마 숫자 만들기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/16922. 로마 숫자 만들기/로마 숫자 만들기.py
+++ 백준/Silver/16922. 로마 숫자 만들기/로마 숫자 만들기.py
@@ -7,7 +7,7 @@ arr=[1,5,10,50]
 
 sum_set = set()
 
-def dfs(depth,idx,ans_list):
+def dfs(idx,ans_list):
 
     if len(ans_list) == n:
         sum_set.add(sum(ans_list))
@@ -15,9 +15,9 @@ def dfs(depth,idx,ans_list):
 
     for i in range(idx,4):
         #ans_list.append(arr[i])
-        dfs(depth+1,i, ans_list+[arr[i]])
+        dfs(i, ans_list+[arr[i]])
         #ans_list.pop()
 
-dfs(0,0,[])
+dfs(0,[])
 
 print(len(sum_set))
