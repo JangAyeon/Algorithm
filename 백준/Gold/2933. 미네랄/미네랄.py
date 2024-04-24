7275dd7e127a9b7680e2d143b065ab1c3d69ab12
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2024-04-24 14:58:53 +0900

    [Gold I] Title: 미네랄, Time: 556 ms, Memory: 34208 KB -BaekjoonHub

--- 백준/Gold/2933. 미네랄/미네랄.py
+++ 백준/Gold/2933. 미네랄/미네랄.py
@@ -2,61 +2,50 @@ import sys
 input = sys.stdin.readline
 from collections import deque
 
-# 왼, 오 순서대로 미네랄을 파괴
-    # 미네랄을 파괴하면 그 즉시 탐색 stop
-# 파괴 후 클러스터 발생 시(공중에 떠있으면 안됨)
-    # - 바닥과 만날 때까지 이동
-    # - 또 다른 미네랄을 만날 때 까지 이동
+n, m = map(int, input().split())
+graph = [list(input().strip()) for _ in range(n)]
+cnt = int(input())
+cmd = list(map(int, input().split()))
 
-R, C = map(int, input().split())
-graph = []
-for i in range(R):
-    graph.append(list(input().strip()))
-
-cnt = int(input()) # 막대 던지는 횟수
-hlist = list(map(int,input().split()))
-
-
-
-def destroy_mi(y, turn): # 미네랄 파괴
-    if turn%2==1:
-        start = C-1
+def destroy(row, turn):
+    if turn%2==1: ## 오른쪽
+        start = m-1
         end = -1
-        gap = -1
-    else:
+        gap =-1
+    else: ## 왼쪽
         start = 0
-        end = C
-        gap = +1
-
-    for i in range(start, end, gap):
-        if graph[y][i]=="x":
-            graph[y][i]="."
+        end = m
+        gap=+1
+    for col in range(start, end, gap):
+        if graph[row][col]=="x":
+            graph[row][col]="."
             break
-    
     return graph
 
-
 def find_cluster(graph):
-    visit = [[False for _ in range(C)] for _ in range(R)]
+    visit = [[False for _ in range(m)] for _ in range(n)]
     dir = [[-1, 0],[1,0],[0,-1],[0,+1]]
     que = deque()
-    for i in range(C):
-        if graph[R-1][i] == 'x':
-            que.append((R-1, i))
+    ## 바닥에서부터 연결된 미네랄 덩이 탐색 (클러스터 탐색) 
+    for i in range(m):
+        if graph[n-1][i] == 'x':
+            que.append((n-1, i))
     while que:
         r, c = que.popleft()
         for dr, dc in dir:
             nr,nc = r+dr,c+dc
-            if not(0<=nr<R) or not(0<=nc<C) or visit[nr][nc]:
+            if not(0<=nr<n) or not(0<=nc<m) or visit[nr][nc]:
                 continue
 
             if graph[nr][nc] == 'x':
                 visit[nr][nc] = True
                 que.append((nr, nc))
 
+    ## 미네랄인데 바닥에서부터 탐색과정에서 방문되지 않은 미네랄이면 공중에 떠있는 미네랄임
+    ## => 움직이기 처리가 필요한 미네랄
     cluster = []
-    for r in range(R-1, -1, -1):
-        for c in range(C):
+    for r in range(n-1, -1, -1):
+        for c in range(m):
             if graph[r][c] == 'x' and not(visit[r][c]):
                 # 클러스터 발생
                 cluster.append([r,c])
@@ -64,31 +53,35 @@ def find_cluster(graph):
         return cluster, 1, visit  # cluster 있음
     else:
         return cluster, 0, visit  # cluster 없음
+    
+def move_cluster(graph, cluster, visited):
+    min_dr = float("inf")
 
-def move_cluster(graph, cluster, visit):
-    down_min = 1e9
-    for r, c in cluster:
-        down_cnt = 0
-        for nr in range(r+1, R):
-            if graph[nr][c] == '.':
-                down_cnt += 1
-            elif graph[nr][c] == 'x' and visit[nr][c]:
+    for r,c in cluster:
+        down = 0
+        for nr in range(r+1, n):
+            if graph[nr][c]==".":
+                down+=1
+            elif graph[nr][c]=="x" and visited[nr][c]:
                 break
-        down_min = min(down_min, down_cnt)
+        min_dr = min(min_dr, down)
+    
     for r, c in cluster:
-        graph[r][c] = '.'
-        graph[r+down_min][c] = 'x'
+        graph[r][c]="."
+        graph[r+min_dr][c]="x"
     return graph
 
 for i in range(cnt): # 막대를 던지는 횟수만큼 반복
     # 1) 미네랄 파괴
-    graph = destroy_mi(R - hlist[i], i) # 바닥부터 1
+    graph = destroy(n - cmd[i], i) # 바닥부터 1
     # 2) 클러스터 조사
-    cluster, check, visit = find_cluster(graph)
+    cluster, check, visited = find_cluster(graph)
     # 3) 클러스터 이동
     if check == 1:
-        graph = move_cluster(graph, cluster, visit)
+        graph = move_cluster(graph, cluster, visited)
 
 
-for i in range(R):
+
+
+for i in range(n):
     print("".join(graph[i]))
