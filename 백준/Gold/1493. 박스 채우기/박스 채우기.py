f779620bd6f02c9ad09c97c4c84580fdb4592953
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2024-05-06 01:02:30 +0900

    [Gold II] Title: 박스 채우기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1493. 박스 채우기/박스 채우기.py
+++ 백준/Gold/1493. 박스 채우기/박스 채우기.py
@@ -1,34 +1,29 @@
 import sys
+input = sys.stdin.readline
 
-# 입력 받기
-input_lines = sys.stdin.readlines()
-l, w, h = map(int, input_lines[0].split())
-totalV = l * w * h
-n = int(input_lines[1])
-arr = []
+l,w,h = map(int, input().split())
+total_v  = l * w * h
+n = int(input())
+cubes = []
+for i in range(n):
+    s, count = map(int, input().split())
+    s = 2**s
+    ## 부피, 한변, 갯수
+    cubes.append([s*s*s, s, count])
 
-# 큐브 정보 입력 및 처리
-for line in input_lines[2: n+2]:
-    a, b = map(int, line.split())
-    s = 2 ** a
-    ## 부피, 갯수, 한변
-    arr.append([s * s * s, b, s])
 
-arr.sort(reverse=True, key=lambda x: x[0])
-
+curr_v = 0
 answer = 0
-total_now = 0
+cubes.sort(reverse=True)
+for v, s, c in cubes:
 
-# 큐브 채우기
-for size, count, s in arr:
-    ## # 다음 순서 = 이전 정육면체 부피의 1/8이므로 이전까지의 개수에 8을 곱해줌 (예 : 4*4*4 1개 = 2*2*2 8개)
-    total_now *= 8
-    cnt_limit = (l //s) * (h // s) * (w // s) - total_now
-    cnt_limit = min(count, cnt_limit)
-    answer += cnt_limit
-    total_now += cnt_limit
+    curr_v*=8
+    curr_c = (l//s)*(w//s)*(h//s)-curr_v
+    curr_c = min(curr_c, c)
+    answer+=curr_c
+    curr_v+=curr_c
 
-if total_now != totalV:
-    print(-1)
-else:
+if curr_v==total_v:
     print(answer)
+else:
+    print(-1)
