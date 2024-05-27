6cc5af3e8e7a74deee29057a46d2f0daf0f623ac
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2024-05-27 22:30:22 +0900

    [level 2] Title: H-Index, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,11 +1,10 @@
 def solution(citations):
     answer = 0
-    for c in range(1,max(citations)+1):
-        score = 0
-        for i in citations:
-            if i>=c:
-                score+=1
-        if c<=(score):
-            answer =  c
+    citations.sort(reverse=True)
+    print(citations)
+    for num, citation in enumerate(citations, start=1):
+        # 피 인용수가 논문의 수랑 같아지는 지점(num은 1부터 시작하도록)
+        if citation >= num: 
+            answer = h_index = num
 
     return answer
