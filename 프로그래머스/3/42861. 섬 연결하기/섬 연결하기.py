b2d189d88f0fdcf2c255e55b42fafe872d9c2ccd
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2024-06-04 19:39:37 +0900

    [level 3] Title: 섬 연결하기, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
@@ -0,0 +1,25 @@
+### 유니온 파인드로 풀어보자
+def find_parent(x, parent):
+    if x!=parent[x]:
+        x = find_parent(parent[x],parent)
+    return parent[x]
+
+def union(a,b,parent):
+    a=find_parent(a,parent)
+    b=find_parent(b,parent)
+    if a<b:
+        parent[b]=a
+    else:
+        parent[a]=b
+
+def solution(n, costs):
+    answer = 0
+    costs.sort(key=lambda x:(x[2]))
+    visited=[False]*(n)
+    parent=[i for i in range(n)]
+    for a,b,c in costs:
+        if find_parent(a, parent)!=find_parent(b,parent):
+            union(a,b,parent)
+            answer+=c
+            
+    return answer
