adec885a38d93b7c783a0cd33b469423fc377c68
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2024-06-11 11:11:03 +0900

    [level 3] Title: 단속카메라, Time: 3.69 ms, Memory: 10.8 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.py
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.py
@@ -1,18 +1,16 @@
-
-
 def solution(routes):
-    answer = 1
     routes.sort()
-    start,end =routes[0]
-
-    for s,e in routes: ## 구간을 좁히고 ## 그 좁은 구간 안에 속하지 못하면 갱신
+    print(routes)
+    start, end=routes[0]
+    answer = 1
+    for s,e in routes[1:]:
+        
         if start<=s<=end:
-            start = max(start, s)
+            start = s
             end = min(end, e)
         else:
             start = s
             end = e
             answer+=1
-        ##print(s,e,start,end,answer)
-
+        ##print(start, end, answer)
     return answer
