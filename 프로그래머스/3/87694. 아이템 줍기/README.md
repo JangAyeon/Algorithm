6081d195ccc15c0a22c64e467783ac4572ad964c
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2024-06-11 11:42:09 +0900

    [level 3] Title: 아이템 줍기, Time: 0.79 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/87694. 아이템 줍기/README.md
+++ 프로그래머스/3/87694. 아이템 줍기/README.md
@@ -1,10 +1,10 @@
 # [level 3] 아이템 줍기 - 87694 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87694) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87694?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 0.27 ms
+메모리: 10.4 MB, 시간: 0.79 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 01일 22:21:09
+2024년 06월 11일 11:42:07
 
 ### 문제 설명
 
--- 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.py
+++ 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.py
@@ -1,55 +1,47 @@
 from collections import deque
 
+dr=[-1,1,0,0]
+dc=[0,0,-1,1]
 
-
-def bfs(start, end, graph):
-    n = len(graph)
-    dr=[1,-1,0,0]
-    dc=[0,0,1,-1]
-    
-    que = deque([start])
-    while que:
-        r, c = que.popleft()
-        if [r,c]==end:
-            return graph[r][c]//2
-        for idx in range(4):
-            nr, nc = r + dr[idx], c+dc[idx]
-            ## 범위 나가는 경우
-            ## 1) 그래프 밖에 2) 도형 영역 밖에 3) 도형 영역 안에
-            if not(0<=nr<n and 0<=nc<n) or graph[nr][nc]==0 or graph[nr][nc]==-1:
-                continue
-            ## 최초로 방문한 경우
-            if graph[nr][nc]==1:
-                graph[nr][nc]= graph[r][c]+1
-                que.append([nr, nc])
-
-    
-    
-
-
-def makeGraph(c1,r1,c2,r2, graph):
+def drawGraph(c1,r1,c2,r2,board):
     for r in range(r1, r2+1):
-        for c in range(c1,c2+1):
-            ## 테두리 좌표 && 다른 도형의 안쪽 아님 
-            if (r1==r or r==r2 or c1==c or c==c2) and graph[r][c]!=0:
-                graph[r][c]=1
+        for c in range(c1, c2+1):
+            if (r==r1 or r==r2 or c1==c or c2==c) and board[r][c]!=0:
+                board[r][c]=1
             else:
-                graph[r][c]=0
-    return graph
+                board[r][c]=0
+    return board
+
+def bfs(start, end,board):
+    que = deque()
+    n = len(board)
+    visited = [[False for _ in range(n)] for _ in range(n)]
+    que.append(start)
+    visited[start[0]][start[1]]=True
+    while que:
+        r,c = que.popleft()
+        if [r,c]==end:
+            print(board[r][c]//2)
+            return board[r][c]//2
+        for idx in range(4):
+            nr, nc = r+dr[idx],c+dc[idx]
+            
+            ## 범위 나감 // 이미 방문함 // 테두리 아님
+            if not(0<=nr<n) or not(0<=nc<n) or board[nr][nc]!=1 or visited[nr][nc]==True:
+                continue
+            ##print(nr, nc, board[nr][nc]+board[r][c] )
+            que.append([nr, nc])
+            visited[nr][nc]=True
+            board[nr][nc]+=board[r][c]
     
 
 
-def solution(rectangle, characterX, characterY, itemX, itemY):
-    answer = 0
-    ## rectangle = [[1,1,7,4],[3,2,5,5]]
-    n=102
-    graph = [[-1]*(n) for _ in range(n)]
-    
-    
-    for c1,r1,c2,r2 in rectangle:
-        graph =makeGraph(2*c1,2*r1,2*c2,2*r2, graph)
-        
-    answer = (bfs( [2*characterY,2*characterX],[2*itemY,2*itemX], graph))
-        
-        
+def solution(rectangles, characterX, characterY, itemX, itemY):
+    n = 51*2
+    ## 아예 테두리=1, 영역 내부=0, 영역 밖=-1
+    board = [[-1 for _ in range(n)] for _ in range(n)]
+    for c1,r1,c2,r2 in rectangles:
+        board = drawGraph(c1*2, r1*2,c2*2,r2*2, board)
+    answer = bfs([characterY*2,characterX*2],[itemY*2,itemX*2] ,board)
+
     return answer
