ecdc7a8529c9b362696a3f7c7895e1d9d263dc8b
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2024-06-20 12:11:19 +0900

    [level 2] Title: 과제 진행하기, Time: 2.58 ms, Memory: 10.7 MB -BaekjoonHub

--- 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.py
+++ 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.py
@@ -1,33 +1,33 @@
-def solution(plans):
-    answer=[]
-    stack =[]
-    n = len(plans)
-    
-    for idx in range(n):
-        subject, hhmm, time = plans[idx]
-        hh, mm= map(int, hhmm.split(":"))
-        time = int(time)
-        plans[idx][1] = hh*60+mm
-        plans[idx][2]=time
-    plans.sort(key=lambda x:x[1])
-    print(plans)
-    for idx in range(n-1):
-        gap = plans[idx+1][1]-plans[idx][1]
-        stack.append([plans[idx][0], plans[idx][2]])
+def timeToMin(time):
+    hh,mm = map(int,time.split(":"))
+    return hh*60+mm
 
-        while gap and stack:
-            if gap>=stack[-1][1]: ## 다음 과제 전까지 현재 과제 다 할 수 있음
-                name, time=stack.pop()
+## 현재 시간과 다음 과제 꼭 시작해야 하는 시간(=gap) 구함
+    ## gap 내에 현재 과제 다 할 수 있는 경우 => 정답에 추가
+    ## gap 내 현재 과제 다 못하는 경우 => 남은 시간 갱신 해 
+
+def solution(plans):
+    answer = []
+    stack = []
+    plans.sort(key=lambda x:x[1]) ## 과제 꼭 시작해야 하는 순서대로 나열
+    
+    for idx in range(0, len(plans)-1):
+        curr_, next_ = plans[idx], plans[idx+1]
+        gap = timeToMin(next_[1])- timeToMin(curr_[1])
+        stack.append([curr_[0], int(curr_[2])])
+        while stack and gap:
+            if stack[-1][-1]<=gap: #
+                name, time = stack.pop()
                 answer.append(name)
                 gap-=time
-            else: ## 다음 과제 전까지 현재 과제 다 끝내지 못함
-                stack[-1][1]-=gap
-                gap = 0
-    answer.append(plans[-1][0])
+            else:
+                stack[-1][-1]-=gap
+                gap=0
+    answer.append(plans[idx+1][0])
     while stack:
-        answer.append(stack.pop()[0])
-    
-                
+        name, time = stack.pop()
+        answer.append(name)
 
 
+        
     return answer
