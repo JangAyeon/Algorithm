7a572fe9ab465e8c3eeb7f2d33394b1145ffb435
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2024-07-01 14:10:01 +0900

    [Silver II] Title: 가장 긴 짝수 연속한 부분 수열 (small), Time: 1156 ms, Memory: 237764 KB -BaekjoonHub

--- 백준/Silver/22857. 가장 긴 짝수 연속한 부분 수열 （small）/가장 긴 짝수 연속한 부분 수열 （small）.py
+++ 백준/Silver/22857. 가장 긴 짝수 연속한 부분 수열 （small）/가장 긴 짝수 연속한 부분 수열 （small）.py
@@ -1,17 +1,25 @@
-n, k = map(int, input().split())
-s = [0] + list(map(int, input().split()))
-dp = [[0 for _ in range(k+1)] for _ in range(n+1)]
+import sys
+input = sys.stdin.readline
 
-for i in range(1, n+1):
-    s[i] %= 2
-    for j in range(k+1):
-        if s[i] == 0: #짝수일 때
-            dp[i][j] = dp[i-1][j] + 1
-        if j != 0 and s[i]: #홀수일 때 
-            dp[i][j] = dp[i-1][j-1]
-            
-result = []
-for i in dp:
-    result.append(i[k])
+n,m =map(int, input().split())
+arr = [0]+list(map(int, input().split()))
+## print(n,m,arr)
 
+#0번 제거, 1번 제거, 2번 제거, m번 제거
+dp = [[0 for _ in range(m+1)] for _ in range(n+1)]
+
+for i in range(1,n+1):
+    s = arr[i]%2 
+    for j in range(m+1):
+        if s==0: ## 짝수인 경우
+            dp[i][j]=dp[i-1][j]+1
+        else: ## 홀수인 경우
+            if j==0:
+                continue
+            dp[i][j]=dp[i-1][j-1]
+
+result = []               
+for v in dp:
+    result.append(v[m])
+    
 print(max(result))
