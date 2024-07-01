4e67b8f323f780a55ed63744cb4c254fda35e820
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2024-07-01 15:27:53 +0900

    [Gold I] Title: 구슬 탈출 4, Time: 56 ms, Memory: 34288 KB -BaekjoonHub

--- 백준/Gold/15653. 구슬 탈출 4/구슬 탈출 4.py
+++ 백준/Gold/15653. 구슬 탈출 4/구슬 탈출 4.py
@@ -2,67 +2,67 @@ import sys
 input = sys.stdin.readline
 from collections import deque
 
-directions =[[-1,0],[1,0],[0,-1],[0,1]]
-
-## 가로, 세로
 n,m = map(int, input().split())
-board = [list(input().strip()) for _ in range(n)]
-visited = [[[[False for _ in range(m)] for _ in range(n)]for _ in range(m)]for _ in range(n)]
-##print(n,m, board)
+graph=[]
 r_start = []
 b_start = []
+visited=[[[[False for _ in range(m)]for _ in range(n)] for _ in range(m)] for _ in range(n)]
+directions  = [[-1,0],[1,0],[0,1],[0,-1]]
+
+
 for i in range(n):
-    for j in range(m):
-        if board[i][j]=="R":
-            r_start=[i,j]
-        elif board[i][j]=="B":
-            b_start=[i,j]
-            
-def bfs(rr,rc,br,bc):
-    que = deque()
-    que.append([rr,rc,br,bc,0])
+    row = list(input().strip())
+    if "R" in row:
+        r_start = [i, row.index("R")]
+    if "B" in row:
+        b_start = [i, row.index("B")]
+    graph.append(row)
+
+##print(r_start, b_start)
+
+
+def bfs(rr, rc, br,bc):
+    que=deque()
+    que.append([rr, rc, br, bc,0])
     visited[rr][rc][br][bc]=True
     while que:
-        rr,rc,br,bc,count=que.popleft()
-        if board[rr][rc]=="O": ## 빨간색: 구멍 도달
+        rr, rc,br,bc,count=que.popleft()
+        
+        if graph[rr][rc]=="O":
             return count
         for dr, dc in directions:
-            ## 빨간색 움직이기
-            nrr, nrc = rr,rc
+            nrr, nrc, nbr, nbc = rr, rc,br,bc
             while True:
-                nrr, nrc = nrr+dr, nrc+dc
-                if board[nrr][nrc]=="#":
+                nrr+=dr
+                nrc+=dc
+                if graph[nrr][nrc]=="O":
+                    break
+                if graph[nrr][nrc]=="#":
                     nrr-=dr
                     nrc-=dc
                     break
-                if board[nrr][nrc]=="O":
-                    break
-            ## 파란색 움직이기
-            nbr, nbc = br,bc
             while True:
-                nbr, nbc = nbr+dr, nbc+dc
-                if board[nbr][nbc]=="#":
+                nbr+=dr
+                nbc+=dc
+                if graph[nbr][nbc]=="O":
+                    break
+                if graph[nbr][nbc]=="#":
                     nbr-=dr
                     nbc-=dc
                     break
-                if board[nbr][nbc]=="O":
-                    break
-            if board[nbr][nbc]=="O":
+            if graph[nbr][nbc]=="O":
                 continue
-            if (nrr == nbr) and (nrc==nbc):
-                ## 빨간 구슬이 더 멀리서 온 경우
-                if abs(nrr-rr)+abs(nrc-rc) > abs(nbr-br)+abs(nbc-bc):
+            if (nrr==nbr) and (nrc==nbc):
+                if abs(rr-nrr)+abs(rc-nrc)>abs(br-nbr)+abs(bc-nbc):
                     nrr-=dr
                     nrc-=dc
                 else:
                     nbr-=dr
                     nbc-=dc
             if not(visited[nrr][nrc][nbr][nbc]):
+                que.append([nrr, nrc, nbr,nbc,count+1])
                 visited[nrr][nrc][nbr][nbc]=True
-                que.append([nrr, nrc, nbr, nbc, count+1])
-                
     return -1
-    
-print(bfs(r_start[0], r_start[1],b_start[0],b_start[1]))
-        
-    
+
+
+print(bfs(r_start[0], r_start[1], b_start[0],b_start[1]))
