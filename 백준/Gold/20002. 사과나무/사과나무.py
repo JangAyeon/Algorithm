3758766ae06275ca49dfb77d5bb6d5e09fe4ebb9
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2024-07-02 12:52:45 +0900

    [Gold V] Title: 사과나무, Time: 4692 ms, Memory: 33164 KB -BaekjoonHub

--- 백준/Gold/20002. 사과나무/사과나무.py
+++ 백준/Gold/20002. 사과나무/사과나무.py
@@ -2,21 +2,25 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-pre = [[-1001]*(n+1) for _ in range(n+1)]
+graph = [[-1000 for _ in range(n+1)]]
 
-# (i, j)까지의 구간합 구하기
-for i in range(1, n+1):
-    arr = list(map(int, input().split()))
-    for j in range(1, n+1):
-        pre[i][j] = pre[i][j-1] + pre[i-1][j] - pre[i-1][j-1] + arr[j-1]
 
-max_profit = pre[0][0]
-# 정사각형으로 자른다.
-for k in range(n):
-    for i in range(1, n-k+1):
-        for j in range(1, n-k+1):
-            # 시작점을 시작점(i, j), 끝점이(i+k, j+k)인 정사각형의 총이익
-            profit = pre[i+k][j+k] - pre[i-1][j+k] - pre[i+k][j-1] + pre[i-1][j-1]
-            max_profit = max(max_profit, profit)    # 최대 총이익 업데이트
+for _ in range(n):
+    row = [-1000]+list(map(int, input().split()))
+    graph.append(row)
 
-print(max_profit)
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        graph[i][j]+=graph[i-1][j]+graph[i][j-1]-graph[i-1][j-1]
+
+
+
+answer = graph[0][0]
+for size in range(1,n+1):
+    for i in range(size,n+1):
+        for j in range(size,n+1):
+            result = graph[i][j]-graph[i-size][j]-graph[i][j-size]+ graph[i-size][j-size]
+            ###print(size,(i,j),result )
+            answer=max(answer, result)
+
+print(answer)
