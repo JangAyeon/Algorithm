f13115afd2469ef99f9e3947e555a59788f03b50
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2024-07-05 17:17:51 +0900

    [Gold IV] Title: 사다리, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/2022. 사다리/사다리.py
@@ -0,0 +1,23 @@
+import sys
+input = sys.stdin.readline
+
+x,y,c = map(float, input().split())
+start, end = 0, min(x,y)
+
+
+def get_c(m):
+    a = (x**2-m**2)**(1/2)
+    b = (y**2-m**2)**(1/2)
+    return a*b/(a+b)
+
+
+result = 0 
+while end-start>1e-6:
+    mid = (start+end)/2
+    if get_c(mid)>=c:
+        start = mid
+        result = mid
+    else:
+        end=mid
+
+print("{}".format(round(result,4)))
