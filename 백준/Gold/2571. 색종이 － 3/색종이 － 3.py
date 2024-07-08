b20576a36e78171dcf9f17afe29bd25ef9d35181
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2024-07-08 10:16:52 +0900

    [Gold IV] Title: 색종이 - 3, Time: 312 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2571. 색종이 － 3/색종이 － 3.py
+++ 백준/Gold/2571. 색종이 － 3/색종이 － 3.py
@@ -1,26 +1,36 @@
-#문제  : https://www.acmicpc.net/problem/2571
-
 import sys
+input = sys.stdin.readline
 
-arr = [[0] * 100 for _ in range(100)] #색종이 도화지에 붙인 상태 변수 선언
-N = int(sys.stdin.readline()) #색종이 갯수
-for _ in range(N):
-    width, height = map(int, sys.stdin.readline().split()) #왼쪽 변 거리, 아래쪽 변 거리
-    for i in range(10): #도화지 붙이기
-        for j in range(10):
-            arr[width+i][height+j] = 1
-
-for x in range(100): #전체 돌며 
-    for y in range(100):
-        if arr[x][y]: #해당 부분인 1이면
-            arr[x][y] += arr[x - 1][y] #각 줄의 높이 구하기
+n = int(input())
+m = 101
+board = [[0 for _ in range(m)] for _ in range(m)]
+for _ in range(n):
+    r,c = map(int, input().split())
+    for dr in range(10):
+        for dc in range(10):
+            board[r+dr][c+dc]=1
 
 
-ans = 0
-for x in range(100):
-    for y in range(100):
-        height = 100 #최대 높이로 임시로 설정
-        for z in range(y, 100): #시작 위치부터 체크해서 가장 낮은 높이 구하기
-            height = min(height, arr[x][z]) #가장 작은 값으로 갱신
-            ans = max(ans, height * (z - y + 1))
-print(ans)
+for r in range(m):
+    for c in range(m):
+        if board[r][c]:
+            board[r][c]+=board[r][c-1]
+
+"""
+for i in board:
+    print(i[3:30])
+
+"""
+
+
+answer = 0
+for c in range(m):
+    for r in range(m):
+        width = m
+        for nr in range(r, m):
+            width = min(width, board[nr][c])
+            answer = max(answer, width*(nr-r+1))
+print(answer)
+
+
+
