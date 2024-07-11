cdbf1267f375412220fe8de1322d659f09c30f8b
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2024-07-11 12:49:26 +0900

    [level 3] Title: 단속카메라, Time: 2.23 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.py
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.py
@@ -1,16 +1,16 @@
 def solution(routes):
     routes.sort()
-    print(routes)
-    start, end=routes[0]
+    ##print(routes)
+    start, end = routes[0]
     answer = 1
-    for s,e in routes[1:]:
-        
+    for s, e in routes[1:]:
         if start<=s<=end:
             start = s
-            end = min(end, e)
+            end=min(end,e)
         else:
-            start = s
-            end = e
+            start=s
+            end=e
             answer+=1
-        ##print(start, end, answer)
+        ##print(start, end)
+
     return answer
