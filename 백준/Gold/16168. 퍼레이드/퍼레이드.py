f0fc46a7dda4dcc0f307cae8b24af2bfd23e3199
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2024-07-11 12:11:23 +0900

    [Gold IV] Title: 퍼레이드, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/16168. 퍼레이드/퍼레이드.py
+++ 백준/Gold/16168. 퍼레이드/퍼레이드.py
@@ -8,7 +8,7 @@ v,e=map(int,sys.stdin.readline().split())
 
 parents=[i for i in range(v+1)]
 
-edge=[[] for _ in range(v+1)]
+graph=[[] for _ in range(v+1)]
 
 def find_parent(x):
     if x!=parents[x]:
@@ -29,30 +29,49 @@ def unionParent(x,y):
     else:
         parents[x]=y
 
+def all_union():
+    base = parents[1]
+    for i in range(2, v+1):
+        if parents[i]!=base:
+            return False
+    return True
+
+
+def able_degree():
+    odd_count =0
+    for i in range(1,v+1):
+        if len(graph[i])%2==1:
+            odd_count+=1
+    if odd_count==0 or odd_count==2:
+        return True
+    
+    else:
+        return False
+
 
 for _ in range(e):
     v1,v2=map(int,sys.stdin.readline().split())
-    edge[v1].append(v2)
-    edge[v2].append(v1)
+    graph[v1].append(v2)
+    graph[v2].append(v1)
     
     if parents[v1]!=parents[v2]:
         unionParent(v1,v2)
 
-
-# 모든 노드가 다같이 연결되었는지 확인 (공통의 부모를 공유하는지 파악)
+flag=False
 base=find_parent(1) # 기준: 1번째 노드의 부모로 설정.
 for i in range(2, v+1):
     if base!=find_parent(i):
         print('NO')
-        exit(0)
+        flag=True
+        break
+if not(flag):
+    odd_cnt=0 # 차수가 홀수인 노드 개수 카운트.
+    for i in range(1,v+1):
+        if len(graph[i])%2==1:
+            odd_cnt+=1
 
-odd_cnt=0 # 차수가 홀수인 노드 개수 카운트.
-for i in range(1,v+1):
-    if len(edge[i])%2==1:
-        odd_cnt+=1
 
-
-if odd_cnt==2 or odd_cnt==0:
-    print('YES')
-else:
-    print('NO')
+    if odd_cnt==2 or odd_cnt==0:
+        print('YES')
+    else:
+        print('NO')
