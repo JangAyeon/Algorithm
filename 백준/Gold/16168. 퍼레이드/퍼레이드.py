6fd1ad93ac780608c34cf337fc672ef8efcd28a2
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2024-07-11 12:16:14 +0900

    [Gold IV] Title: 퍼레이드, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/16168. 퍼레이드/퍼레이드.py
+++ 백준/Gold/16168. 퍼레이드/퍼레이드.py
@@ -20,7 +20,7 @@ def find_parent(x):
 
 
 
-def unionParent(x,y):
+def union(x,y):
     x=find_parent(x)
     y=find_parent(y)
 
@@ -29,10 +29,13 @@ def unionParent(x,y):
     else:
         parents[x]=y
 
+
+
+
 def all_union():
-    base = parents[1]
+    base = find_parent(1)
     for i in range(2, v+1):
-        if parents[i]!=base:
+        if find_parent(i)!=base:
             return False
     return True
 
@@ -55,23 +58,15 @@ for _ in range(e):
     graph[v2].append(v1)
     
     if parents[v1]!=parents[v2]:
-        unionParent(v1,v2)
-
-flag=False
-base=find_parent(1) # 기준: 1번째 노드의 부모로 설정.
-for i in range(2, v+1):
-    if base!=find_parent(i):
-        print('NO')
-        flag=True
-        break
-if not(flag):
-    odd_cnt=0 # 차수가 홀수인 노드 개수 카운트.
-    for i in range(1,v+1):
-        if len(graph[i])%2==1:
-            odd_cnt+=1
+        union(v1,v2)
 
 
-    if odd_cnt==2 or odd_cnt==0:
-        print('YES')
+
+
+if all_union():
+    if able_degree():
+        print("YES")
     else:
-        print('NO')
+        print("NO")
+else:
+    print("NO")
