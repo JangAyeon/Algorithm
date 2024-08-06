39384295a76731e4774f090f1fe5eb73748d753d
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2024-08-06 14:22:05 +0900

    [level 2] Title: 카펫, Time: 0.32 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.js
+++ 프로그래머스/2/42842. 카펫/카펫.js
@@ -1,11 +1,11 @@
 function solution(brown, yellow) {
     let answer = [];
-    for(let i=1;i<=(yellow**(1/2));i++){
+    for(let i=1;i<=Math.sqrt(yellow);i++){
         
    
         if((i+(yellow/i))*2==(brown-4)){
-            answer = [i+2, yellow/i+2]
-            answer.sort((a,b)=>b-a)
+            answer = [yellow/i+2,i+2]
+            // answer.sort((a,b)=>b-a)
             // console.log(answer)
         }
 
