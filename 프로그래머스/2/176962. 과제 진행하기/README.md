d7e4272dfe1623330c750424a35742e02ba6c12a
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2024-08-06 12:52:34 +0900

    [level 2] Title: 과제 진행하기, Time: 2.71 ms, Memory: 34.7 MB -BaekjoonHub

--- 프로그래머스/2/176962. 과제 진행하기/README.md
+++ 프로그래머스/2/176962. 과제 진행하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 과제 진행하기 - 176962 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/176962?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/176962) 
 
 ### 성능 요약
 
-메모리: 34.8 MB, 시간: 11.25 ms
+메모리: 34.7 MB, 시간: 2.71 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 04일 19:30:43
+2024년 08월 06일 12:52:32
 
 ### 문제 설명
 
--- 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.js
+++ 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.js
@@ -1,46 +1,47 @@
-function formatTime(time) {
-    const [HH, MM] = time.split(":")
-    return parseInt(HH) * 60 + parseInt(MM)
+function formatMM(time){
+    const [hh,mm]=time.split(":").map(Number)
+    return hh*60+mm
 }
 
-function formatPlans(plans) {
-    plans = plans.map((item) => [item[0], formatTime(item[1]), parseInt(item[2])])
-    return plans.sort((a, b) => a[1] - b[1])
-}
 
+function parseArr(plans){
+    const arr = []
+    for (let [a,b,c] of plans){
+        arr.push([a,formatMM(b),Number(c)])
+    }
+    arr.sort((a,b)=>a[1]-b[1])
+    return arr
+}
 
 function solution(plans) {
-    const stack = [] // 하다가 중단된 과제
-    // 과제들 시작 시간 기준으로 나열해야 함
-    plans = formatPlans(plans)
-    console.log(plans)
+    plans = parseArr(plans)
     const answer = []
-    let idx;
-    for(idx=0; idx<plans.length-1;idx++){
-        const curr_ = plans[idx]
-        const next_ = plans[idx+1]
-        let mustStart = next_[1]-curr_[1]
-        stack.push([curr_[0], curr_[2]])
-        while(stack.length && mustStart){
-            if(stack[stack.length-1][1]<=mustStart){
-                const item = stack.pop() 
+    const waits=[]
+    let gapTime
+    for(let i=0;i<plans.length-1;i++){
+        const curr_ = plans[i]
+        const next_ = plans[i+1]
+        gapTime=next_[1]-curr_[1]
+        waits.push([curr_[0], curr_[2]])
+        while(waits.length && gapTime){
+            
+            if(waits[waits.length-1][1]-gapTime>0){
+                waits[waits.length-1][1]-=gapTime
+                gapTime=0
+            }else{
+                const item = waits.pop()
                 answer.push(item[0])
-                mustStart-=item[1]
-            }
-            else{
-                stack[stack.length-1][1]-=mustStart
-                mustStart=0
+                gapTime-=item[1]
             }
             
         }
+
+
     }
     answer.push(plans[plans.length-1][0])
-    while(stack.length){
-        const item = stack.pop()
-        answer.push(item[0])
+    while(waits.length){
+        answer.push(waits.pop()[0])
     }
 
-   console.log(answer)
-    return answer
+    return answer;
 }
-
