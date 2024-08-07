51240d033bb554f0291c4091d7fc8ad8fe2e6a00
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2024-08-07 13:28:53 +0900

    [level 5] Title: 방의 개수, Time: 587.34 ms, Memory: 122 MB -BaekjoonHub

---
+++ 프로그래머스/5/49190. 방의 개수/방의 개수.js
@@ -0,0 +1,35 @@
+
+    
+
+
+function solution(arrows) {
+    const directions=[
+    
+    [-1,0],[-1,1],[0,1],[1,1],[1,0],[1,-1],[0,-1],[-1,-1]
+    
+]
+   
+    const edges = new Map()
+    const nodes = new Map()
+    let r =0
+    let c = 0
+    let answer = 0
+    nodes.set(`${r}_${c}`, true)
+    for(let dir of arrows){
+for(let i=0;i<2;i++){
+            let [nr, nc] = [r+directions[dir][0], c+directions[dir][1]]
+        let hasEdges = edges.has(`${r}_${c}_${nr}_${nc}`)
+        let hasNode = nodes.has(`${nr}_${nc}`)
+        if(hasNode && !hasEdges){
+            answer+=1
+        }
+        nodes.set(`${nr}_${nc}`,true)
+        edges.set(`${r}_${c}_${nr}_${nc}`,true)
+        edges.set(`${nr}_${nc}_${r}_${c}`,true)
+        r=nr
+        c=nc
+}
+
+    }
+    return answer;
+}
