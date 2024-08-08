b9e1cb9163773e7c8c345ba5f106a3331f701579
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2024-08-08 13:39:53 +0900

    [level 5] Title: 방의 개수, Time: 666.94 ms, Memory: 82.8 MB -BaekjoonHub

--- 프로그래머스/5/49190. 방의 개수/방의 개수.js
+++ 프로그래머스/5/49190. 방의 개수/방의 개수.js
@@ -1,34 +1,33 @@
-
-    
-
-
 function solution(arrows) {
-    const directions=[
-    
-    [-1,0],[-1,1],[0,1],[1,1],[1,0],[1,-1],[0,-1],[-1,-1]
-    
-]
-   
-    const edges = new Map()
+    // 두칸씩 이동
+    // 방문한 노드
+    // 방문 루트 : a->b, b->a
+    let answer = 0;
     const nodes = new Map()
+    const routes = new Map()
     let [r,c] = [0,0]
-    let answer = 0
     nodes.set(`${r}_${c}`, true)
-    for(let dir of arrows){
-for(let i=0;i<2;i++){
-            let [nr, nc] = [r+directions[dir][0], c+directions[dir][1]]
-        let hasEdges = edges.has(`${r}_${c}_${nr}_${nc}`)
+    const directions = [
+        [-1,0], [-1,1],[0,1],[1,1],[1,0],[1,-1],[0,-1],[-1,-1]
+    ]
+    
+    for(const d of arrows){
+        const [dr, dc] = directions[d]
+        for (let i=0;i<2;i++){
+            let [nr, nc] = [r+dr,c+dc]
+        let hasEdges = routes.has(`${r}_${c}_${nr}_${nc}`)
         let hasNode = nodes.has(`${nr}_${nc}`)
         if(hasNode && !hasEdges){
             answer+=1
         }
-        nodes.set(`${nr}_${nc}`,true)
-        edges.set(`${r}_${c}_${nr}_${nc}`,true)
-        edges.set(`${nr}_${nc}_${r}_${c}`,true)
-        r=nr
-        c=nc
-}
-
+            nodes.set(`${nr}_${nc}`, true)
+            routes.set(`${r}_${c}_${nr}_${nc}`, true)
+            routes.set(`${nr}_${nc}_${r}_${c}`, true)
+            r=nr
+            c=nc
+        }
+      
     }
+   
     return answer;
 }
