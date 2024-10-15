0a542ed3815b29bc3a4fc5059b23bcb76313d66b
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2024-10-16 01:06:32 +0900

    [Gold IV] Title: 부분합, Time: 104 ms, Memory: 42204 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.py
+++ 백준/Gold/1806. 부분합/부분합.py
@@ -1,26 +1,28 @@
-#문제 : https://www.acmicpc.net/problem/1806
-
 import sys
+input = sys.stdin.readline
 
-N, S = map(int, sys.stdin.readline().split())
-arr = list(map(int, sys.stdin.readline().split()))
+n,m = map(int, input().split())
+arr = list(map(int, input().split()))
 
-start, end, sum = 0, 0, 0 #시작 부분, 끝 부분, 시작 ~ 끝 부분 함
-min_ = 100001
+
+start = end = total = 0
+length = float("inf")
+
 
 while True:
-    # 현재 부분 합 > = s 
-    if sum >= S:
-        sum -= arr[start]   # 앞에서 원소 한 개 삭제 : start의 위치 1 증가
-        start += 1
-        min_ = min(min_, end - start + 1)   # 가장 작은 길이의 부분 합 : start~end까지가 현재 최소값보다 작으면 최소값 갱신
-  
-    elif end == N:    # end=N되면 탐색 종료
-        break
-    else: # 현재 부분 합 < s 
-        sum += arr[end]     # 뒤로 원소 1개 추가 : end 위치 1 증가
-        end += 1
+    if m<=total: ## 좀 빼야 함
+        total-=arr[start]
+        start+=1
+        length = min(length, end-start+1)
+    else:
+        if n==end:
+            break
+        total+=arr[end]
+        end+=1
 
-if min_ == 100001:  # 초기 min값과 같다면 sum이 S를 넘지 못한 것
-    min_ = 0
-print(min_)
+        
+if length==float("inf"):
+    length = 0
+print(length)
+    
+    
