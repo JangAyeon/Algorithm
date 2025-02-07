696a1f3393561b92ee37f5b0e590420b5f1c7c3a
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2025-02-08 01:51:20 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 796 ms, Memory: 146200 KB -BaekjoonHub

--- 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.js
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.js
@@ -1,39 +1,43 @@
 const readline = require("readline")
 const rl = readline.createInterface({
+
     input: process.stdin,
     output: process.stdout
+
+
 })
 
 const lines = []
 
+
 rl.on("line", (line) => {
     const s = line.trim()
     lines.push(s)
-}).on(("close"), () => {
-    const N = Number(lines[0])
+}).on("close", () => {
+    const n = Number(lines[0])
     const arr = lines[1].split(" ").map(Number)
-    let answer = [arr[0]];
+    const answer = [arr[0]]
 
-    for (let i = 1; i < N; i++) {
-        if (answer[answer.length - 1] < arr[i]) {
+    for(let i=1;i<n;i++){
+        if(answer[answer.length-1]<arr[i]){
             answer.push(arr[i])
-        } else {
-            let idx = binarySearch(answer, arr[i])
+        }
+        else{
+            const idx =  binarySearch(answer, arr[i])
             answer[idx] = arr[i]
         }
     }
+    // console.log(answer)
 
 
-    function binarySearch(arr, target) {
+
+    function binarySearch(arr, target){
         let start = 0
-        let end = arr.length - 1
-        while (start <= end) {
-            let mid = parseInt((start + end) / 2)
-            if (arr[mid] < target) {
-                start = mid + 1
-            } else {
-                end = mid - 1
-            }
+        let end = arr.length-1
+        while(start<=end){
+            let mid = parseInt((start+end)/2)
+            if(target>arr[mid]){start=mid+1}
+            else{end=mid-1}
         }
         return start
     }
