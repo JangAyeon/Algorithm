76881baa4497a094e494569e857abb1f9119b875
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2025-02-09 15:32:37 +0900

    [Gold I] Title: 부분수열의 합 2, Time: 1616 ms, Memory: 171824 KB -BaekjoonHub

--- 백준/Gold/1208. 부분수열의 합 2/부분수열의 합 2.js
+++ 백준/Gold/1208. 부분수열의 합 2/부분수열의 합 2.js
@@ -4,70 +4,61 @@ const rl = readline.createInterface({
     output: process.stdout
 })
 
-
 const lines = []
 
+
 rl.on("line", (line) => {
-
-    const s = line.trim()
-    lines.push(s)
-
+    lines.push(line.trim())
 }).on("close", () => {
-    const [N, M] = lines[0].split(" ").map(Number)
-    const half = Math.floor(N / 2)
-    const arr = lines[1].split(" ").map(Number)
-    const leftArr = arr.slice(0, half)
-    const rightArr = arr.slice(half)
-    const leftDic = {}
-    let answer = 0
+        const [N, S] = lines[0].split(" ").map(Number)
+        const half = Math.floor(N / 2)
+        const arr = lines[1].split(" ").map(Number)
+        const leftArr = arr.slice(0, half)
+        const rightArr = arr.slice(half)
+        let answer = 0
+        const leftDic = {}
+        // console.log(leftArr, rightArr, N, S, arr)
+        leftSearch(0, 0)
 
-    leftSearch(0, 0);
+        function leftSearch(sum_, index) {
+            if (index == half) {
+                return
+            }
+            const curr = leftArr[index]
+            leftSearch(sum_ + curr, index + 1)
+            leftSearch(sum_, index + 1)
+            if (leftDic[sum_ + curr]) {
+                leftDic[sum_ + curr]++
+            } else {
+                leftDic[sum_ + curr] = 1
+            }
+            if (sum_ + curr === S) {
+                answer++
+            }
 
-    function leftSearch(sum_, index) {
-        if (index == half) {
-            return
         }
-        const curr = leftArr[index]
-        leftSearch(sum_ + curr, index + 1)
-        leftSearch(sum_, index + 1)
-        if (leftDic[sum_ + curr]) {
-            leftDic[sum_ + curr]++
-        } else {
-            leftDic[sum_ + curr] = 1
+        rightSearch(0, 0)
+
+        function rightSearch(sum_, index) {
+            if (index == N - half) {
+                return
+            }
+            const curr = rightArr[index]
+            rightSearch(sum_ + curr, index + 1)
+            rightSearch(sum_, index + 1)
+            if (leftDic[S - (sum_ + curr)]) {
+                answer += (leftDic[S - (sum_ + curr)])
+            }
+            if (sum_ + curr === S) {
+                answer++
+            }
+
         }
-        if (M === sum_ + curr) {
-            answer++
-        }
+        console.log(answer)
+
     }
 
-    rightSearch(0, 0);
-
-    function rightSearch(sum_, index) {
-        if (index == (N - half)) {
-            return
-        }
-        const curr = rightArr[index]
-        rightSearch(sum_ + curr, index + 1)
-        rightSearch(sum_, index + 1)
-        const restValue = M - (sum_ + curr)
-        if (leftDic[restValue]) {
-            answer += leftDic[restValue]
-        }
-        if (sum_ + curr == M) {
-            answer++
-        }
-    }
-
-    console.log(answer)
 
 
 
-
-})
-
-
-/**
-5 0
-0 0 0 0 0
-ans: 31
-**/
+)
