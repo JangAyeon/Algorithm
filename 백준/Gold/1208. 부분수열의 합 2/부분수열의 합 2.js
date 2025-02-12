d2e8a4358465da86eb4a640c31fdc82e1c36d940
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2025-02-13 06:59:25 +0900

    [Gold I] Title: 부분수열의 합 2, Time: 404 ms, Memory: 39756 KB -BaekjoonHub

--- 백준/Gold/1208. 부분수열의 합 2/부분수열의 합 2.js
+++ 백준/Gold/1208. 부분수열의 합 2/부분수열의 합 2.js
@@ -1,69 +1,64 @@
 const readline = require("readline")
 const rl = readline.createInterface({
+
     input: process.stdin,
     output: process.stdout
+
 })
+
 
 const lines = []
 
 
 rl.on("line", (line) => {
     lines.push(line.trim())
+
 }).on("close", () => {
-        const [N, S] = lines[0].split(" ").map(Number)
-        const half = Math.floor(N / 2)
-        const arr = lines[1].split(" ").map(Number)
-        const leftArr = arr.slice(0, half)
-        const rightArr = arr.slice(half)
-        let answer = 0
-        const leftDic = new Map()
-        // console.log(leftArr, rightArr, N, S, arr)
-        leftSearch(0, 0)
+    const [N, S] = lines[0].split(" ").map(Number)
+    const arr = lines[1].split(" ").map(Number)
+    const half = Math.floor(N / 2)
+    const leftArr = arr.slice(0, half)
+    const rightArr = arr.slice(half)
+    const leftDic = new Map()
+    let answer = 0
 
-        function leftSearch(sum_, index) {
-            if (index == half) {
-                return
-            }
-            const curr = leftArr[index]
-            leftSearch(sum_ + curr, index + 1)
-            leftSearch(sum_, index + 1)
+    leftSearch(0, 0)
 
-            const value = (leftDic.get(sum_+curr)??0)+1
-            leftDic.set(sum_+curr,value )
-            // if (leftDicsum_ + curr]) {
-            //     leftDic[sum_ + curr]++
-            // } else {
-            //     leftDic[sum_ + curr] = 1
-            // }
-            if (sum_ + curr === S) {
-                answer++
-            }
-
+    function leftSearch(sum_, index) {
+        if (index === half) {
+            return
         }
-        rightSearch(0, 0)
-
-        function rightSearch(sum_, index) {
-            if (index == N - half) {
-                return
-            }
-            const curr = rightArr[index]
-            rightSearch(sum_ + curr, index + 1)
-            rightSearch(sum_, index + 1)
-            const rest = S - (sum_ + curr)
-            if (leftDic.has(rest)) {
-                const value = leftDic.get(rest)
-                answer += (value)
-            }
-            if (sum_ + curr === S) {
-                answer++
-            }
-
+        const curr = leftArr[index]
+        leftSearch(sum_ + curr, index + 1)
+        leftSearch(sum_, index + 1)
+        const k = sum_ + curr
+        const v = (leftDic.get(k) ?? 0) + 1
+        leftDic.set(k, v)
+        if (k === S) {
+            answer++
         }
-        console.log(answer)
+    }
 
+    rightSearch(0, 0)
+
+    function rightSearch(sum_, index) {
+        if (index === (N - half)) {
+            return
+        }
+        const curr = rightArr[index]
+        rightSearch(sum_ + curr, index + 1)
+        rightSearch(sum_, index + 1)
+        const k = S - (sum_ + curr)
+        if (leftDic.has(k)) {
+            const v = leftDic.get(k)
+            answer += v
+        }
+        if (k === 0) {
+            
+            answer++
+        }
     }
 
 
-
-
-)
+    console.log( answer)
+})
