6b9c39705988a87d147c755176f59cd3a0506a28
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2025-02-13 07:14:42 +0900

    [Gold III] Title: 두 배열의 합, Time: 260 ms, Memory: 39296 KB -BaekjoonHub

--- 백준/Gold/2143. 두 배열의 합/두 배열의 합.js
+++ 백준/Gold/2143. 두 배열의 합/두 배열의 합.js
@@ -4,41 +4,41 @@ const rl = readline.createInterface({
     output: process.stdout
 })
 
-
-
 const lines = []
-
-
 
 
 rl.on("line", (line) => {
     lines.push(line.trim())
 }).on("close", () => {
-    const T = +lines[0]
-    const N = +lines[1]
-    const a = lines[2].split(' ').map(Number)
-    const K = +lines[3]
-    const b = lines[4].split(' ').map(Number)
-
-    let answer = 0;
-
-    let A = new Map();
-    for (let i = 0; i < N; i++) {
-        let sum = 0;
-        for (let j = i; j < N; j++) {
-            sum += a[j]
-            A.set(sum, (A.get(sum) ?? 0) + 1)
-        }
-    }
-
+    const N = Number(lines[0])
+    const K = Number(lines[1])
+    const arr1 = lines[2].split(" ").map(Number)
+    const P = Number(lines[3])
+    const arr2 = lines[4].split(" ").map(Number)
+    const arr1Dic = new Map()
+    let answer = 0
+    // 구간 합 arr1
     for (let i = 0; i < K; i++) {
-        let sum = 0;
+        let pre_sum = 0
         for (let j = i; j < K; j++) {
-            sum += b[j];
-            answer += A.get(T - sum) ?? 0;
+            pre_sum += arr1[j]
+            const v = (arr1Dic.get(pre_sum) ?? 0) + 1
+            arr1Dic.set(pre_sum, v)
         }
     }
 
-    console.log(answer)
 
+    // 구간 합 arr2
+        for (let i = 0; i < P; i++) {
+        let pre_sum = 0
+        for (let j = i; j < P; j++) {
+            pre_sum += arr2[j]
+            const v = (arr1Dic.get(N-pre_sum) ?? 0) 
+            answer+=v
+           
+        }
+    }
+    //console.log("close", N, K, P)
+    //console.log("close", arr1, arr2)
+    console.log(answer)
 })
