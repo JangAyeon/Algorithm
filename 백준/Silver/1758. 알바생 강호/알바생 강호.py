90caea2bd32e94b0c53dd2fd21997494e8cbb7be
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2025-02-26 07:00:36 +0900

    [Silver IV] Title: 알바생 강호, Time: 92 ms, Memory: 35108 KB -BaekjoonHub

--- 백준/Silver/1758. 알바생 강호/알바생 강호.py
+++ 백준/Silver/1758. 알바생 강호/알바생 강호.py
@@ -1,20 +1,13 @@
 import sys
-from itertools import permutations
 input = sys.stdin.readline
 
 n = int(input())
+arr = [int(input()) for _ in range(n)]
+arr.sort(reverse=True)
 answer=0
-c =sorted([int(input()) for _ in range(n)], reverse=True)
-#print(arr, lst)
-#print(c)
 
-for idx, value in enumerate(c,1):
-    score = value-(idx-1)
-    if score<0: score=0
-    answer+=score
-    #idx=idx+1
-    #answer+=value-(idx-1)
-
-        
+for idx, v in enumerate(arr,1):
+    temp = v-(idx-1)
+    if temp>0:
+        answer+=temp
 print(answer)
-        
