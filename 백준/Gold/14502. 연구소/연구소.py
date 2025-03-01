25a0231304193d734b94e9ec5795d8736faa1cdd
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2025-03-01 13:42:55 +0900

    [Gold IV] Title: 연구소, Time: 2240 ms, Memory: 36152 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -1,68 +1,54 @@
-import sys
-from copy import deepcopy
+from itertools import combinations
 from collections import deque
-input = sys.stdin.readline
+import copy
 
-row, col = map(int, input().split())
-graph = [list(map(int, input().split())) for _ in range(row)]
+# 입력 받기
+N, M = map(int, input().split())
+lab = [list(map(int, input().split())) for _ in range(N)]
 
-# 0은 빈 칸, 1은 벽, 2는 바이러스
-dr = [-1,1,0,0]
-dc = [0,0,-1,1]
-answer = 0
+# 빈 칸과 바이러스 위치 저장
+empty_spaces = []
+virus_positions = []
 
-# virusList
-virusList = []
+for i in range(N):
+    for j in range(M):
+        if lab[i][j] == 0:
+            empty_spaces.append((i, j))
+        elif lab[i][j] == 2:
+            virus_positions.append((i, j))
 
-# 깊이 우선 탐색으로 각 바이러스 사방에 퍼지게
-def spread(r,c, temp):
-    for idx in range(4):
-        nr = r + dr[idx]
-        nc = c + dc[idx]
-        
-        if 0<=nr<row and 0<=nc<col:
-            if temp[nr][nc] == 0: # 빈칸인 경우
-                temp[nr][nc] = 2
-                spread(nr,nc, temp)
-                
-                
-# 현재 맵에서 안전 영역 크기 계산
-def get_size(temp):
-    size = 0
-    for i in range(row):
-        for j in range(col):
-            if temp[i][j] == 0:
-                size+=1
-    #print(size)
-    return size
-    
-def dfs(start,count):
-    global answer
-    
-    if count==3:
-        # 그래프 복사
-        temp  = deepcopy(graph)
-        
-        # 바이러스 퍼트리기 
-        for i in range(len(virusList)):
-            r,c = virusList[i]
-            spread(r,c, temp)
+# 4방향 이동 정의
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
-        answer = max(answer, get_size(temp))
-        return
-    
-    for i in range(start, row*col):
-        x = int(i/col)
-        y = int(i%col)
-        if graph[x][y] == 0:
-            graph[x][y] = 1
-            dfs(i+1,count+1)
-            graph[x][y] = 0
-            
-for i in range(row):
-    for j in range(col):
-        if graph[i][j] == 2:
-            virusList.append([i,j])
+def spread_virus(lab_map):
+    """ 바이러스를 퍼뜨리고 남은 안전 영역 크기를 반환 """
+    queue = deque(virus_positions)
+    temp_map = copy.deepcopy(lab_map)  # 원본 배열 보존
 
-dfs(0,0)
-print(answer)
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx, ny = x + dx[i], y + dy[i]
+            if 0 <= nx < N and 0 <= ny < M and temp_map[nx][ny] == 0:
+                temp_map[nx][ny] = 2
+                queue.append((nx, ny))
+
+    return sum(row.count(0) for row in temp_map)  # 남은 안전 영역 크기 반환
+
+# 벽을 세울 수 있는 모든 조합 시도
+max_safe_area = 0
+for walls in combinations(empty_spaces, 3):
+    # 벽 세우기
+    for x, y in walls:
+        lab[x][y] = 1
+
+    # 바이러스 퍼트리고 안전 영역 계산
+    max_safe_area = max(max_safe_area, spread_virus(lab))
+
+    # 벽 복구
+    for x, y in walls:
+        lab[x][y] = 0
+
+# 결과 출력
+print(max_safe_area)
