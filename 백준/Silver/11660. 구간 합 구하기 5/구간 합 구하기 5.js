977c0ac582809ee0f5212b32bc019b1668711206
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2025-07-13 15:32:38 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 664 ms, Memory: 90768 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.js
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.js
@@ -1,26 +1,43 @@
-const readline = require('readline');
+const readline = require("readline");
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+  input: process.stdin,
+  output: process.stdout,
 });
 
 const input = [];
-rl.on('line', line => input.push(line)).on('close', () => {
-   // 수 갯수 N, 합 구하는 횟수 M
-  	const [N,M] = input[0].split(" ").map(Number)
-    const dp = [new Array(N+1).fill(0),...input.slice(1,N+1).map(item => [0,...item.split(" ").map(Number)])]
-    const T = input.slice(N+1).map(item => item.split(" ").map(Number))
-    // console.log(dp)
-    for(let i=1;i<=N;i++){
-        for(let j=1; j<=N;j++){
-            const num = dp[i-1][j]+dp[i][j-1]-dp[i-1][j-1]+dp[i][j]
-            dp[i][j]=num
-        }
+
+rl.on("line", (line) => input.push(line)).on("close", () => {
+  const [N, M] = input[0].split(" ").map(Number);
+
+  // 1. 입력받은 원본 표
+  const original = input.slice(1, 1 + N).map((row) => row.split(" ").map(Number));
+
+  // 2. 2차원 누적합 배열 (0행, 0열 padding 포함)
+  const prefixSum = Array.from({ length: N + 1 }, () => Array(N + 1).fill(0));
+
+  // 3. 누적합 배열 채우기
+  for (let i = 1; i <= N; i++) {
+    for (let j = 1; j <= N; j++) {
+      prefixSum[i][j] =
+        prefixSum[i - 1][j] +
+        prefixSum[i][j - 1] -
+        prefixSum[i - 1][j - 1] +
+        original[i - 1][j - 1]; // 주의: original은 0-indexed
     }
-    for(let[x1,y1,x2,y2] of T){
-        const answer = dp[x2][y2]-(dp[x1-1][y2]+dp[x2][y1-1]-dp[x1-1][y1-1])
-        console.log(answer)
-    }
-    // console.log(N,M, T)
-    
+  }
+
+  // 4. 쿼리 처리
+  const queries = input.slice(1 + N).map((line) => line.split(" ").map(Number));
+
+  const results = [];
+  for (const [x1, y1, x2, y2] of queries) {
+    const sum =
+      prefixSum[x2][y2] -
+      prefixSum[x1 - 1][y2] -
+      prefixSum[x2][y1 - 1] +
+      prefixSum[x1 - 1][y1 - 1];
+    results.push(sum);
+  }
+
+  console.log(results.join("\n"));
 });
