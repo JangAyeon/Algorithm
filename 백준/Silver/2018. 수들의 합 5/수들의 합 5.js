fe363ff98dfd59db6a48d68d17de4a50751d9d41
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2025-07-28 06:36:10 +0900

    [Silver V] Title: 수들의 합 5, Time: 220 ms, Memory: 9752 KB -BaekjoonHub

--- 백준/Silver/2018. 수들의 합 5/수들의 합 5.js
+++ 백준/Silver/2018. 수들의 합 5/수들의 합 5.js
@@ -1,33 +1,32 @@
 const readline = require("readline")
 const rl = readline.createInterface({
-    input:process.stdin,
-    output:process.stdout
+    input: process.stdin,
+    output: process.stdout
 })
 
 const input = []
-rl.on("line", line =>input.push(line)).on("close", ()=>{
+rl.on("line", line => input.push(line)).on("close", () => {
 
-const N = +input[0];
-
-let count = 0;
-let start = 1;
-let end = 1;
-let total = 1;
-
-while (end <= N) {
-  if (total < N) {
-    end++;
-    total += end;
-  } else if (total === N) {
-    count++;
-    end++;
-    total += end;
-  } else {
-    total -= start;
-    start++;
-  }
-}
-
-console.log(count);
-
+    const N = +input[0]
+    // const arr = Array.from({length:N}).map((_,idx)=>idx+1)
+    let [start, end,sum, count] = [1,1,1,0]
+    while(end<=N){
+        // console.log(start, end,sum)
+        if(sum<N){
+            end++
+            sum+=end
+        }
+        else if(sum>N){
+            sum-=start
+            start++
+            
+        }
+        else{
+            count++
+            end++
+            sum+=end
+            
+        }
+    }
+    console.log(count)
 })
