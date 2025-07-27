f1d838f27f12f56bd07d7af94aa31d4396291853
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2025-07-28 06:17:48 +0900

    [Gold IV] Title: 좋다, Time: 196 ms, Memory: 10252 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.js
+++ 백준/Gold/1253. 좋다/좋다.js
@@ -1,40 +1,42 @@
-const readline = require("readline");
+const readline = require("readline")
 const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+    input: process.stdin,
+    output: process.stdout
+})
 
-const input = [];
+const input = []
+rl.on("line", line => input.push(line)).on("close", () => {
 
-rl.on("line", (line) => input.push(line)).on("close", () => {
-  const N = +input[0];
-  const arr = input[1].split(" ").map(Number).sort((a, b) => a - b);
-
-  let goodCount = 0;
-
-  for (let i = 0; i < N; i++) {
-    const target = arr[i];
-    let start = 0;
-    let end = N - 1;
-
-    while (start < end) {
-              const sum = arr[start] + arr[end];
-        if(sum==target){
-            if(i!=start && i!=end){
-                goodCount++
-                break
+    const N = +input[0]
+    const arr = input[1].split(" ").map(Number).sort((a, b) => a - b)
+    let answer= 0 
+    function twoPointer(target, index) {
+        let [start, end] = [0, arr.length - 1]
+        while (start < end) {
+            const sum = arr[start] + arr[end]
+            if (sum === target) {
+                if (start != index && end != index) {
+                    return true
+                } else if (start == index) {
+                    start++
+                } else {
+                    end--
+                }
+            } else if (sum < target) {
+                start++
+            } else {
+                end--
             }
-            else if(i==start){start++}
-            else if (i==end){end--}
+
         }
+        return false
 
- else if (sum < target) {
-        start++;
-      } else {
-        end--;
-      }
     }
-  }
 
-  console.log(goodCount);
-});
+    for (let index = 0; index < arr.length ; index++) {
+        // console.log(arr[index], twoPointer(arr[index], index))
+        twoPointer(arr[index], index) && answer++
+    }
+    console.log(answer)
+
+})
