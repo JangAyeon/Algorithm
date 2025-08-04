9f058d3f074a4de5c64dcfd36a5c4762897a1e99
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2025-08-04 19:55:44 +0900

    [Silver III] Title: 소수 구하기, Time: 204 ms, Memory: 24500 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.js
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.js
@@ -1,37 +1,28 @@
-// 3 16
-
-const readline = require("readline")
+const readline = require("readline");
 const rl = readline.createInterface({
-
     input: process.stdin,
-    output: process.stdout
+    output: process.stdout,
+});
 
-})
+const lines = [];
+rl.on("line", (line) => lines.push(line)).on("close", () => {
+    const [M, N] = lines[0].split(" ").map(Number);
+    const isNotPrime = Array(N + 1).fill(false);
+    isNotPrime[0] = true;
+    isNotPrime[1] = true;
 
-
-const lines = []
-rl.on("line", line => lines.push(line)).on("close", () => {
-
-    const [n, m] = lines[0].split(" ").map(Number)
-    // console.log(n, m)
-  	const arr = Array.from({length:m+1}).fill(false)
-    arr[0]=true
-    arr[1]=true
-    for(let idx=2;idx<parseInt((m)**(1/2))+1;idx++){
-        let step = 2
-        while(idx*step<arr.length){
-                   if(!arr[step*idx]){arr[step*idx]=true}
-        step++ 
+    for (let i = 2; i * i <= N; i++) {
+        if (!isNotPrime[i]) {
+            for (let j = i * i; j <= N; j += i) {
+                isNotPrime[j] = true;
+            }
         }
-
     }
-      const answer =  []
-    for(let idx=0;idx<arr.length;idx++){
-        !arr[idx]&& idx>=n && idx<=m && answer.push(idx)
+
+    const result = [];
+    for (let i = M; i <= N; i++) {
+        if (!isNotPrime[i]) result.push(i);
     }
-  
-    console.log(answer.join("\n"))
-    // console.log(arr, arr.length)
 
-
-})
+    console.log(result.join("\n"));
+});
