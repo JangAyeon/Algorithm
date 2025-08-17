aef8c606ba4471c7ab56340241d0c7b7b8c06a87
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2025-08-18 00:05:42 +0900

    [Gold V] Title: 회의실 배정, Time: 436 ms, Memory: 42656 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.js
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.js
@@ -1,89 +1,25 @@
 const readline = require("readline")
-
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 })
 
 const lines = []
-rl.on("line", (line) => {
+rl.on("line", line => lines.push(line)).on("close", () => {
 
-    lines.push(line.trim())
-
-
-}).on("close", () => {
-
-    const N = Number(lines[0])
-    const times = lines.slice(1).map((r) => r.split(" ").map(Number))
-
-    const schedules = []
-
-
-    for (let t of times) {
-        const [start, end] = t
-
-        schedules.push({
-            gap: end - start,
-            start,
-            end
-
-        })
-
-    }
-
-    // 정렬 : 시간 순 ---> 종료 순
-
-    schedules.sort((a, b) => {
-        if (a.start === b.start) {
-            return a.end - b.end
-        }
-        return a.start - b.start
-    })
-
-
-
+    const N = +lines[0]
+    const arr = lines.slice(1).map((item)=>item.split(" ").map(Number))
+    arr.sort((a,b)=>(a[1]-b[1]||a[0]-b[0]))
     let answer = 0
-    let count = 1
-    let start = -Infinity
-    let end = Infinity
-
-
-
-
-    for (let t of schedules) {
-        const [s, e] = [t.start, t.end]
-        // 갭 줄이기
-        if (start < s && e < end) {
-
-            start = s
-            end = e
-
-        } else if (end <= s) {
-            count += 1
-            start = s
-            end = e
-
-
+    let end = -1
+    for(let e of arr){
+        if(e[0]>=end){
+            answer++
+            end = e[1]
         }
-
-
-
-        //console.log([s,e], count)
     }
+    
+    // console.log(arr)
+    console.log(answer)
 
-    console.log(count)
 })
-
-/**
-5
-1 3
-1 4
-4 4
-4 4
-4 5
-
-
-answ: 4
-
-
-**/
