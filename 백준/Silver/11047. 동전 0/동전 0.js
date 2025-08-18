7075ecf70af1d3f530ad9c4d1f983ae0af71dfd9
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2025-08-18 14:54:16 +0900

    [Silver IV] Title: 동전 0, Time: 120 ms, Memory: 9744 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.js
+++ 백준/Silver/11047. 동전 0/동전 0.js
@@ -1,34 +1,22 @@
 const readline = require("readline")
+
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 })
 
-
 const lines = []
-
-rl.on("line", (line) => {
-    const s = line.trim()
-    lines.push(s)
-}).on("close", () => {
-    const [n, m] = lines[0].split(" ").map(Number)
-    const arr = lines.slice(1).map((r) => Number(r))
-    arr.sort((a, b) => -a + b)
-    //console.log(n, m, arr)
-    let a = m
+rl.on("line", line => lines.push(line)).on("close", () => {
+    let [N ,M]= lines[0].split(" ").map(Number)
+    const arr = lines.slice(1).map(Number).sort((a,b)=>b-a)
     let answer = 0
-    for (let q of arr) {
-        const k = parseInt(a / q)
-        a -= q * k
-        //console.log(q, a, k)
-        answer += k
-
-        if (a <= 0) {
-            break
-        }
-
+    for(let e of arr){
+        const k = Math.floor(M/e)
+        M -= e * k
+        answer+=k
+        // console.log(k, M, answer)
     }
-    console.log(answer)
 
+    console.log(answer)
 
 })
