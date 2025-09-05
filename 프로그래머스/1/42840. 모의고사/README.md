91f48a4199c94ab47f3bf463f4730e28103a9ac8
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2025-09-06 00:28:39 +0900

    [level 1] Title: 모의고사, Time: 6.17 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/README.md
+++ 프로그래머스/1/42840. 모의고사/README.md
@@ -1,10 +1,10 @@
 # [level 1] 모의고사 - 42840 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840?language=javascript) 
 
 ### 성능 요약
 
-메모리: 38.6 MB, 시간: 17.08 ms
+메모리: 36.8 MB, 시간: 6.17 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 21일 15:52:58
+2025년 09월 06일 00:28:36
 
 ### 문제 설명
 
--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -1,26 +1,28 @@
 function solution(answers) {
-    const p1 =[1, 2, 3, 4, 5]
-    const p2 =[2, 1, 2, 3, 2, 4, 2, 5 ]
-    const p3 =[3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
-    const players = [p1,p2,p3]
-    const n = answers.length
-    const scores = new Array(3).fill(0)
-    const answer = []
-    for(const [i, answer] of answers.entries()){
-        for(const [j, p] of players.entries()){
-            if(answer == p[i % p.length]){
-                scores[j]+=1
+    const picks = [[1, 2, 3, 4, 5],
+                   [2, 1, 2, 3, 2, 4, 2, 5],
+                   [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+                 ]
+    const P = picks.length
+    let scores = [0,0,0]
+    for(let idx = 0;idx<P;idx++){
+        const pick = picks[idx]
+        for(let i=0;i<answers.length;i++){
+            if(pick[i%pick.length] == answers[i]){
+                
+                scores[idx]+=1
             }
+            
         }
     }
-    const maxScore = Math.max(...scores)
-    // console.log(scores,maxScore)
-    for(let i=0; i<scores.length;i++){
-        if (scores[i]==maxScore){
-            answer.push(i+1)    
+    const max_score = Math.max(...scores)
+   
+    const answer = []
+    for (let idx = 0;idx<scores.length;idx++){
+        if(scores[idx]==max_score){
+            answer.push(idx+1)
         }
         
     }
-
     return answer;
 }
