0dac9295be88cf175455c527fcbb9271caeb30f7
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2025-09-06 11:29:27 +0900

    [level 2] Title: 모음 사전, Time: 2.07 ms, Memory: 9.51 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/README.md
+++ 프로그래머스/2/84512. 모음 사전/README.md
@@ -1,10 +1,10 @@
 # [level 2] 모음 사전 - 84512 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/84512) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/84512?language=python3) 
 
 ### 성능 요약
 
-메모리: 34.5 MB, 시간: 7.51 ms
+메모리: 9.51 MB, 시간: 2.07 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 09월 06일 11:05:18
+2025년 09월 06일 11:29:25
 
 ### 문제 설명
 
--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -1,27 +1,16 @@
-
-
 def solution(word):
-    v = ["A","E","I","O","U"]
-    n = len(v)
-    visited = [[False for _ in range(n)] for _ in range(n)]
     answer = 0
-    flag = False
-    def dfs(words,dept):
-        nonlocal answer
-        nonlocal flag
-        print(words, dept, answer)
-
-        if "".join(words)==word:
-            print(answer, dept)
-            flag = True
+    N = len(word)
+    arr = []
+    dictionary = [ 'A', 'E', 'I', 'O', 'U']
+    def dfs(lst):
+        if(len(dictionary)==len(lst)):
             return
-        if len(words)>n or dept>=n:
-            return
-        for i in range(n):
-            if not(flag) and not visited[dept][i]:
-                visited[dept][i]=True
-                answer+=1
-                dfs(words+[v[i]], dept+1)
-                visited[dept][i]=False
-    dfs([],0)
-    return answer
+        for i in range(len(dictionary)):
+            w = lst + [dictionary[i]]
+            arr.append("".join(w))
+            dfs(w)
+    dfs([])
+    ## print(arr)
+    print(arr.index(word))
+    return arr.index(word)+1
