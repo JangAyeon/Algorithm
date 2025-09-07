81f124440d1cdf079dd5b4426a06da32f792e6f5
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2025-09-07 13:55:38 +0900

    [level 4] Title: 징검다리, Time: 340.70 ms, Memory: 11.2 MB -BaekjoonHub

--- 프로그래머스/4/43236. 징검다리/징검다리.py
+++ 프로그래머스/4/43236. 징검다리/징검다리.py
@@ -1,33 +1,30 @@
 def solution(distance, rocks, n):
-    answer = -float("inf")
-    start,end= 0,distance
+    answer = 0
+    start, end = 0, distance
     rocks.append(distance)
     rocks.sort()
-    while start<=end:
-        mid = (start+end)//2
-        curr =0 
-        remove_cnt = 0
-        gap_min = float("inf")
-        for rock in rocks:
-
-            gap = rock-curr
-            if gap <mid:
-                
-                remove_cnt +=1
+    ### 주어진 gap으로 제거된 바위 갯수 구하는 함수
+    def getCount(dist):
+        curr = 0
+        removed = 0
+        gap_min = 1000000000
+        for next_ in rocks:
+            gap = next_-curr
+            if(dist>gap):
+                removed+=1
             else:
-                curr = rock
-                gap_min=min(gap, gap_min)
-        
-        ## 제거 횟수가 주어진 횟수보다 큰 경우
-        ## 제거 횟수를 줄이려면 거리를 더 줄여야 함
-        if remove_cnt>n:
+                curr = next_
+                gap_min = min(gap_min, gap)
+                
+        return [removed, gap_min]
+    
+    while(start<=end):
+        mid=(start+end)//2
+        count, gap_min = getCount(mid)
+        if count>n:
             end = mid-1
-
-            ##answer = max(answer, max_gap)
-        ## 제거 횟수가 주어진 횟수보다 더 작은 경우
-        ## 제거 횟수를 늘리려면 주어진 횟수를 더 키워야 함
         else:
             start = mid+1
             answer = max(answer, gap_min)
-           
+                
     return answer
