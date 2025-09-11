6f25908f6c738302b5cef04a1f495a1037729e79
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2025-09-11 23:36:06 +0900

    [level 3] Title: 섬 연결하기, Time: 0.37 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.js
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.js
@@ -1,32 +1,27 @@
-
-function find_parents(node, parents){
-    if(node!=parents[node]){
-        node = find_parents(parents[node],parents)
-    }
-    return parents[node]
-}
-
-function union(a,b, parents){
-    a = find_parents(a, parents)
-     b = find_parents(b, parents)
-    if(a<b){
-        parents[b] =a
-    }
-    else{
-        parents[a] =b
-    }
-}
-
-
-function solution(n, costs) {
+function solution(N, costs) {
     var answer = 0;
-    costs.sort((a,b)=>+a[2]-b[2])
-    const parents = Array.from({length:n+1},(_,i)=>i)
-
-    for(let cost of costs){
-        if(find_parents(cost[0],parents)!=find_parents(cost[1], parents)){
-            union(cost[0],cost[1],parents)
-            answer+=cost[2]
+    costs.sort((a,b)=>a[2]-b[2])
+    const parents = Array.from({length:N}).fill(0).map((_,idx)=>idx)
+    function find(x){
+        if(parents[x]!=x){
+            parents[x]=find(parents[x])
+        }
+        return parents[x]
+    }
+    function union(a,b){
+        a = find(a)
+        b = find(b)
+        if(a!=b){
+            parents[b] = parents[a]
+            return true
+        }
+        return false
+    }
+    for (let cost of costs){
+        const [a,b,c] = cost
+        // console.log(a,b, parents[a],parents[b])
+        if(union(a,b)){
+            answer+=c
         }
     }
     return answer;
