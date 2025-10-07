c5ef355bdaa8e9dd57d9c018e264bb96ce985463
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2025-10-07 13:03:19 +0900

    [level 3] Title: 순위, Time: 42.59 ms, Memory: 38.2 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.js
+++ 프로그래머스/3/49191. 순위/순위.js
@@ -1,29 +1,29 @@
 function solution(N, results) {
-    const answer = []
+    let answer = 0;
     const graph = Array.from({length:N+1},()=>Array.from({length:N+1}).fill(0))
     for(let [a,b] of results){
         graph[a][b]=1
     }
+    
     for(let c=1;c<N+1;c++){
         for(let a=1;a<N+1;a++){
             for(let b=1;b<N+1;b++){
-                if(graph[a][c] && graph[c][b]){
-                    graph[a][b]=1
-                }
+                if(graph[a][c] && graph[c][b]){graph[a][b]=1}
             }
         }
     }
+    
     for(let i=1;i<N+1;i++){
-        let count=0
+        let count = 0
         for(let j=1;j<N+1;j++){
             if(i==j){continue}
-            if(graph[i][j]||graph[j][i]){
+            if(graph[i][j] || graph[j][i]){
                 count+=1
             }
         }
         if(count==N-1){
-            answer.push(i)
+            answer+=1
         }
     }
-    return answer.length;
+    return answer;
 }
