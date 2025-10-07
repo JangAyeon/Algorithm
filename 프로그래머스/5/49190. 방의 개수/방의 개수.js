40591ca59676cd214f5eb4a709cda1210a43e929
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2025-10-07 12:25:54 +0900

    [level 5] Title: 방의 개수, Time: 413.56 ms, Memory: 106 MB -BaekjoonHub

--- 프로그래머스/5/49190. 방의 개수/방의 개수.js
+++ 프로그래머스/5/49190. 방의 개수/방의 개수.js
@@ -1,54 +1,47 @@
 function solution(arrows) {
-    const directions = [
-        [-1, 0],  // 0: 위
-        [-1, 1],  // 1: 오른쪽 위
-        [0, 1],   // 2: 오른쪽
-        [1, 1],   // 3: 오른쪽 아래
-        [1, 0],   // 4: 아래
-        [1, -1],  // 5: 왼쪽 아래
-        [0, -1],  // 6: 왼쪽
-        [-1, -1]  // 7: 왼쪽 위
-    ];
-
-    // 현재 위치
-    let [r, c] = [0, 0];
-    const nodes = new Map();  // 방문한 노드
-    const routes = new Map(); // 방문한 간선 (양방향 저장)
-    let answer = 0;
-
-    // 시작점 방문 처리
-    nodes.set(`${r}_${c}`, true);
-
-    for (let dir of arrows) {
-        const [dr, dc] = directions[dir];
-
-        // 2단계 이동 (대각선 교차 방지)
-        for (let i = 0; i < 2; i++) {
-            const [nr, nc] = [r + dr, c + dc];
-
-            const currKey = `${r}_${c}`;
-            const nextKey = `${nr}_${nc}`;
-            const routeKey = `${currKey}_${nextKey}`;
-            const reverseKey = `${nextKey}_${currKey}`;
-
-            const hasNode = nodes.get(nextKey);
-            const hasRoute = routes.get(routeKey);
-
-            // 이미 방문한 노드지만, 처음 가는 간선이면 방이 새로 생김
-            if (hasNode && !hasRoute) {
-                answer += 1;
+    
+    const directions=[
+        [-1,0],// 0
+        [-1,1],// 1
+        [0,1], // 2
+        [1,1], // 3
+        [1,0], // 4
+        [1,-1],// 5
+        [0,-1], // 6
+        [-1,-1] // 7
+    ]
+    const nodes = new Map()
+    const routes = new Map()
+    let [r,c] = [0,0] 
+    nodes.set(`${r}_${c}`,true)
+    let answer = 0
+    
+    for(let dir of arrows){
+        const [dr, dc] = directions[dir]
+        // console.log(dir)
+        
+        for(let i=0;i<2;i++){
+            const [nr, nc] = [r+dr, c+dc]
+            const currentKey = `${r}_${c}`
+            const nextKey = `${nr}_${nc}`
+            const currentRoute = `${currentKey}_${nextKey}`
+            const reverseRoute = `${nextKey}_${currentKey}`
+            const hasNode = nodes.get(nextKey)
+            const hasRoute = routes.get(currentRoute)
+            // console.log(hasNode, hasRoute)
+            if(hasNode & !hasRoute){
+                answer+=1
             }
-
-            // 방문 처리
-            nodes.set(nextKey, true);
-            routes.set(routeKey, true);
-            routes.set(reverseKey, true);
-
-            // 이동
-            r = nr;
-            c = nc;
+            
+            
+            routes.set(currentRoute, true)
+            routes.set(reverseRoute,true)
+            nodes.set(nextKey, true)
+            
+            r = nr
+            c = nc
+            
         }
     }
-
     return answer;
 }
