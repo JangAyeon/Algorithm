ca9bf3e9c3c17952965975ac26d9b23af3b01b61
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2025-10-08 12:56:45 +0900

    [level 2] Title: 피로도, Time: 23.84 ms, Memory: 38.4 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/README.md
+++ 프로그래머스/2/87946. 피로도/README.md
@@ -1,10 +1,10 @@
 # [level 2] 피로도 - 87946 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946?language=javascript) 
 
 ### 성능 요약
 
-메모리: 9.25 MB, 시간: 47.90 ms
+메모리: 38.4 MB, 시간: 23.84 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 09월 06일 01:35:22
+2025년 10월 08일 12:56:43
 
 ### 문제 설명
 
--- 프로그래머스/2/87946. 피로도/피로도.js
+++ 프로그래머스/2/87946. 피로도/피로도.js
@@ -1,30 +1,42 @@
 function solution(k, dungeons) {
-    var answer = -1;
-    /*
-     최소 필요 피로도 <- 시작을 위함
-     소모 피로도 <- 끝나면 사용되는 피로도
-    */
-    const n = dungeons.length
-    const visited = Array.from({length:n}).fill(false)
-    function dfs(dept, lst,power){
-        answer = Math.max(answer, lst.length)
-        if(dept==n){
-            console.log(lst,power)
-            
+    const N = dungeons.length
+    const visited = Array.from({length:N}).fill(false)
+    let answer = 0
+    //  ["최소 필요 피로도", "소모 피로도"] 
+    function isAble(max_strength,arr){
+        let strength = max_strength
+        for(let idx of arr){
+            const d = dungeons[idx]
+            // console.log(d)
+            // console.log(strength, arr)
+            if(d[0]>strength){
+                return false
+            }
+            strength -=d[1]
+        }
+        return true
+    }
+    
+    function dfs(node,level, route){
+        // console.log(route,isAble(k,route), level)
+        if(isAble(k,route) && route.length>answer){
+            answer = route.length
+        }
+        if(level==N){
             return
         }
-        for(let i=0;i<n;i++){
-            if(visited[i] ||power -dungeons[i][0]<0 )continue
+        for(let i=0;i<N;i++){
+            if(visited[i]){continue}
             visited[i]=true
-            dfs(dept+1, [...lst, i],power -dungeons[i][1])
+            dfs(i, level+1, [...route,i])
             visited[i]=false
         }
     }
-    for(let start=0;start<n;start++){
-        visited[start] = true
-        dfs(1, [start],k-dungeons[start][1])
-        visited[start]=false
-    }
     
+    for(let i=0;i<N;i++){
+        visited[i]=true
+        dfs(i, 1, [i])
+        visited[i]=false
+    }
     return answer;
 }
