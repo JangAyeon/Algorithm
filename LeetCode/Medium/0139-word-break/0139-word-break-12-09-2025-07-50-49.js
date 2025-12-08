2515708c0317aa89bb236919bd2eaa8cc6af9ec2
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2025-12-09 07:50:49 +0900

    Time: 2 ms (87.25%), Space: 55.1 MB (89.83%) - LeetHub

---
+++ LeetCode/Medium/0139-word-break/0139-word-break-12-09-2025-07-50-49.js
@@ -0,0 +1,25 @@
+function wordBreak(s ,wordDict) {
+  const memo = {};
+
+  const dfs = (start) => {
+    if (start in memo) return memo[start];
+
+    if (start === s.length) {
+      memo[start] = true;
+      return true;
+    }
+
+    for (const word of wordDict) {
+      if (s.substring(start, start + word.length) === word) {
+        if (dfs(start + word.length)) {
+          memo[start] = true;
+          return true;
+        }
+      }
+    }
+    memo[start] = false;
+    return false;
+  };
+
+  return dfs(0);
+}
